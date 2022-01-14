fe112ff903a353477425f61b3f395e28af3d39d2
Topdevbeast <topdevbeast@gmail.com>
2022-01-14 12:38:53 -0800

    initial implementations
    adds calculateFee, isWhitelisted, calcAmounts, and getQuantity
    as well as their associated storage variables

--- contracts/stakingNFT/SwapFeeReward.sol
+++ contracts/stakingNFT/SwapFeeReward.sol
@@ -9,10 +9,13 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
     address factory;
     address router;
     address targetToken;
-    address targetAuraNFT;
+    address targetAuraToken;
+
+    uint auraWagerOnSwap;
 
     struct PairsList {
         address pair;
+        uint percentReward;
         bool enabled;
     }
 
@@ -24,19 +27,19 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
         address _factory,
         address _router, 
         address _targetToken,
-        address _targetAuraNFT
+        address _targetAuraToken
     ) {
         require(
             _factory != address(0)
             && _router != address(0)
             && _targetToken != address(0)
-            && _targetAuraNFT != address(0),
+            && _targetAuraToken != address(0),
             "Address cannot be zero."
         );
         factory = _factory;
         router = _router;
         targetToken = _targetToken;
-        targetAuraNFT = _targetAuraNFT;
+        targetAuraToken = _targetAuraToken;
     }
 
     /**
@@ -47,5 +50,50 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
         address pair = AuraLibrary.pairFor(factory, a, b);
         PairsList storage pool = pairsList[pairOfPid[pair]];
         _pairExists = (pool.pair == pair);
+    }
+    
+    /**
+     * TODO
+     */
+    function calculateFee(address account, address input, address output, uint amount) 
+        public
+        view
+        returns(
+            uint feeInAURA,
+            uint feeInUSD,
+            uint pointsAccrued
+        )
+    {
+        uint swapFee = AuraLibrary.getSwapFee(factory, input, output); 
+        address pair = AuraLibrary.pairFor(factory, input, output);
+        PairsList memory pool = pairsList[pairOfPid[pair]];
+
+        if (pool.pair == pair && pool.enabled && isWhitelisted(input) && isWhitelisted(output)) {
+            (uint feeAmount, uint pointAmount) = calcAmounts(amount, account);
+            feeInAURA = getQuantity(output, feeAmount / swapFee, targetToken) * pool.percentReward / 100;
+            feeInUSD = getQuantity(output, pointAmount / auraWagerOnSwap, targetAuraToken);
+            pointsAccrued = getQuantity(targetToken, feeInAURA, targetAuraToken);
+        }
+    }
+
+    /**
+     * TODO
+     */
+    function isWhitelisted(address token) public view returns(bool _isWhitelisted) {
+        // TODO - Implement
+    }
+
+    /**
+     * TODO
+     */
+    function calcAmounts(uint amount, address account) internal view returns(uint feeAmount, uint auraAmount) {
+        // TODO - Implement
+    }
+
+    /**
+     * TODO
+     */
+    function getQuantity(address outputToken, uint outputAmount, address anchorToken) public view returns(uint quantity) {
+        // TODO - Implement
     }
 }
