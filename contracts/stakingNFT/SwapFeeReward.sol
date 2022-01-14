e01a1ce698bc580f2d0c723f3b4eaa23f08b508c
Topdevbeast <topdevbeast@gmail.com>
2022-01-14 11:29:23 -0800

    clear compiler errors

--- contracts/stakingNFT/SwapFeeReward.sol
+++ contracts/stakingNFT/SwapFeeReward.sol
@@ -6,14 +6,16 @@ import '@rari-capital/solmate/src/utils/ReentrancyGuard.sol';
 import '@rari-capital/solmate/src/tokens/ERC20.sol';
 
 interface ISwapFactory {}
-interface ISwapPair {}
+interface ISwapPair {
+    function swapFee() external view returns (uint32);
+}
 interface IAuraToken {}
 interface IAuraNFT {}
 
 contract SwapFeeReward is Ownable, ReentrancyGuard {
     address factory;
     address router;
-    bytes CODE_HASH;
+    bytes32 CODE_HASH;
     IAuraToken auraToken;
     IAuraNFT auraNFT;
     // TODO - Add an oracle 
@@ -23,13 +25,13 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
     constructor(
         address _factory,
         address _router, 
-        bytes _CODE_HASH,
+        bytes32 _CODE_HASH,
         IAuraToken _auraToken,
         IAuraNFT _auraNFT,
         // TODO - Add an oracle 
         address _targetToken,
         address _targetAuraNFT
-    ) public {
+    ) {
         require(
             _factory != address(0)
             && _router != address(0)
@@ -37,10 +39,10 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
             && _targetAuraNFT != address(0),
             "Address cannot be zero."
         );
-        factory = _factory,
-        router = _router,
-        CODE_HASH = _CODE_HASH,
-        auraToken = _auraToken,
+        factory = _factory;
+        router = _router;
+        CODE_HASH = _CODE_HASH;
+        auraToken = _auraToken;
         auraNFT = _auraNFT;
         // TODO - assign oracle
         targetToken = _targetToken;
@@ -48,7 +50,9 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
     }
 
     /**
-     * @return the addresses `a` and `b` sorted in ascending order.
+     * @dev returns the token addresses `a` and `b` sorted in ascending order.
+     * @return c the smaller of the two token addresses.
+     * @return d the larger of the two token addresses. 
      */
     function sortAddresses(address a, address b) public pure returns(address c, address d) {
         require(a != b, "Addresses are identical.");
@@ -57,23 +61,27 @@ contract SwapFeeReward is Ownable, ReentrancyGuard {
     }
 
     /**
-     * @return the pair of addresses `a` and `b` as a single hashed address.
+     * @return hashed the pair of addresses `a` and `b` as a single hashed address.
      */
     function hashPair(address a, address b) public view returns(address hashed) {
         // Sort first to establish consistency across calls since hashing (a, b) != (b, a).
         (address c, address d) = sortAddresses(a, b);
-        hashed = address(uint(keccak256(abi.encodePacked(
+        // TODO - confirm that converting from uint256 -> uint160 -> address results in acceptable behavior.
+        //        It's a work-around since uint256 -> address is not permitted. 
+        //        It should be fine as long as the address isn't supposed to point at an 
+        //        already existing address.
+        hashed = address(uint160(uint(keccak256(abi.encodePacked(
                 hex"ff",
                 factory,
-                keccack256(abi.encodePacked(c, d)),
+                keccak256(abi.encodePacked(c, d)),
                 CODE_HASH
-            ))));
+            )))));
     }
 
     /**
-     * @return the fee incurred for swapping tokens `a` and `b`.
+     * @return swapFee the fee incurred for swapping tokens `a` and `b`.
      */
     function getSwapFee(address a, address b) internal view returns(uint swapFee) {
-        swapFee = uint(1000) - (ISwapPair(hashPair(a, b).swapfee());
+        swapFee = uint(1000) - ISwapPair(hashPair(a, b)).swapFee();
     }
 }
