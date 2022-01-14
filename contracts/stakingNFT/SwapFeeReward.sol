8764615eecea4a6b775972895f29bdb354adf97f
Topdevbeast <topdevbeast@gmail.com>
2022-01-14 12:05:43 -0800

    [WIP] Feature/34 swap fee reward (#35)
    * Update AuraCallee (#24)
    
    * Add factory contract and rename other contracts to comply with Aura
    
    * Add AuraCallee
    
    * swapFeeReward first commit
    
    * add contract imports
    
    * sketch interfaces
    
    * define initial constructor and storage variables
    
    * implement sortTokens
    
    * implement hashPair
    
    * implement getSwapFee
    
    * clear compiler errors
    
    * clean up comments
    
    * remove duplicate functionst already defined in AuraLibrary
    
    * delete unnecessary code
    
    There were significant duplicated functions, interface definitions, and variables.
    Removes those and will only re-add as it becomes necessary.
    
    Co-authored-by: 0xCryptoBilly <97250049+0xCryptoBilly@users.noreply.github.com>

---
+++ contracts/stakingNFT/SwapFeeReward.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0;
+
+import "../libraries/AuraLibrary.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@rari-capital/solmate/src/utils/ReentrancyGuard.sol';
+
+contract SwapFeeReward is Ownable, ReentrancyGuard {
+    address factory;
+    address router;
+    address targetToken;
+    address targetAuraNFT;
+
+    constructor(
+        address _factory,
+        address _router, 
+        address _targetToken,
+        address _targetAuraNFT
+    ) {
+        require(
+            _factory != address(0)
+            && _router != address(0)
+            && _targetToken != address(0)
+            && _targetAuraNFT != address(0),
+            "Address cannot be zero."
+        );
+        factory = _factory;
+        router = _router;
+        targetToken = _targetToken;
+        targetAuraNFT = _targetAuraNFT;
+    }
+}
