bb35636a9d17c04c5446cc8aa3f5008d6bd8d1bc
Topdevbeast <topdevbeast@gmail.com>
2022-01-14 09:47:07 -0800

    sketch interfaces

--- contracts/stakingNFT/SwapFeeReward.sol
+++ contracts/stakingNFT/SwapFeeReward.sol
@@ -3,6 +3,12 @@ pragma solidity >= 0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import '@rari-capital/solmate/src/utils/ReentrancyGuard.sol';
+import '@rari-capital/solmate/src/tokens/ERC20.sol';
+
+interface ISwapFactory {}
+interface ISwapPair {}
+interface IAuraToken {}
+interface IAuraNFT {}
 
 contract SwapFeeReward is Ownable, ReentrancyGuard {
 
