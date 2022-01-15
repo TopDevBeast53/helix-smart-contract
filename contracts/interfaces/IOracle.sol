5a8227a9bc66565fd379b9d10f483b25a069a79c
Topdevbeast <topdevbeast@gmail.com>
2022-01-15 09:24:38 -0800

    [WIP] Feature/34 swap fee reward (#36)
    * Update AuraCallee (#24)
    
    * Add factory contract and rename other contracts to comply with Aura
    
    * Add AuraCallee
    
    * swapFeeReward first commit
    
    * add contract imports
    
    * sketch interfaces
    
    * define initial constructor and storage variables
    
    * implement sortTokens
    
    * implement hashPair
    
    * implement getSwapFee
    
    * clear compiler errors
    
    * clean up comments
    
    * remove duplicate functionst already defined in AuraLibrary
    
    * delete unnecessary code
    
    There were significant duplicated functions, interface definitions, and variables.
    Removes those and will only re-add as it becomes necessary.
    
    * implement pairExists
    
    * initial implementations
    
    adds calculateFee, isWhitelisted, calcAmounts, and getQuantity
    as well as their associated storage variables
    
    * update comment
    
    * implement AddressWhitelist contract
    
    * integrate whitelist
    
    * implement getAmounts
    
    * rename contract
    
    * implement getQuantity
    
    * add IOracle interface
    
    * integrate oracle into getQuantity
    
    * implement swap
    
    * implement accureAura and implement and integrage IAuraNFT
    
    * implement IAuraToken interface
    
    * implement withdraw
    
    * remove TODO natspec stubs
    
    * implement phase and phasePoints
    
    * implement setters and getters
    
    * add todo
    
    * add another todo
    
    * fix deleted NatSpec for remove
    
    Co-authored-by: 0xCryptoBilly <97250049+0xCryptoBilly@users.noreply.github.com>

---
+++ contracts/interfaces/IOracle.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+interface IOracle {
+    function consult(address tokenIn, uint amountIn, address tokenOut) external view returns(uint amountOut);
+}
