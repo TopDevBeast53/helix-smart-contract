72d6266ed722ebe9b84c34f66599d61f82ded0ab
Topdevbeast <topdevbeast@gmail.com>
2022-01-18 11:26:32 -0800

    partial integration of routerFixture
    integration of routerFixture incomplete because router contract deployment throws out of gas error

--- mocha_tests/SwapFeeRewardsWithAP.test.ts
+++ mocha_tests/SwapFeeRewardsWithAP.test.ts
@@ -4,19 +4,20 @@ import { Web3Provider} from 'ethers/providers';
 import { solidity, loadFixture, deployContract, MockProvider } from 'ethereum-waffle';
 
 import SwapFeeRewardsWithAP from '../build/contracts/SwapFeeRewardsWithAP.json';
-import { factoryFixture } from './shared/swapFixtures';
+import { factoryFixture, routerFixture } from './shared/swapFixtures';
 
 use(solidity);
 
 describe('SwapFeeRewardsWithAP', () => {
-    const overrides = { gasLimit: 6500000 };
+    const overrides = { gasLimit: 6700000 };
     const [wallet, walletTo] = new MockProvider().getWallets();
     let contract: Contract;
 
     let factory;
+    let router;
 
     // TODO - Replace with actual contract implementations.
-    let router = '0x90BBC489677C87e26361f665ad3e26E18b063551';
+    //let router = '0x90BBC489677C87e26361f665ad3e26E18b063551';
     let targetToken = '0x4cfdC5Cf9659A57671DAa395AE3F043f8dFB9957';
     let targetAPToken = '0x8f593d9fb3adBDFffBFDb3212BEA73f3DA0d8d30';
     let oracle = '0xae0463B8A46aD8981FF7BD776511cEdC4c5D72f9';
@@ -30,11 +31,12 @@ describe('SwapFeeRewardsWithAP', () => {
 
     beforeEach(async () => {
         let factory = await loadFixture(factoryFixture);
+        let router = await loadFixture(routerFixture);
 
         // Deploy new contract.
         contract = await deployContract(wallet, SwapFeeRewardsWithAP, [
             factory.address, 
-            router, 
+            router.address, 
             targetToken, 
             targetAPToken, 
             oracle, 
