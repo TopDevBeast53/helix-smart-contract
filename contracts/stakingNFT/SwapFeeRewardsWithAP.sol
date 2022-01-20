00a2f1afc89d50acc0517958726e8f0a171cffd9
Topdevbeast <topdevbeast@gmail.com>
2022-01-19 22:18:53 -0800

    update auraNFT.accruePoints

--- contracts/stakingNFT/SwapFeeRewardsWithAP.sol
+++ contracts/stakingNFT/SwapFeeRewardsWithAP.sol
@@ -338,7 +338,7 @@ contract SwapFeeRewardsWithAP is Ownable, ReentrancyGuard {
         if (quantity > 0) {
             totalAccruedAP += quantity;
             if (totalAccruedAP <= phaseAP * maxAccruedAPInPhase) {
-                auraNFT.accrueAP(account, quantity);
+                auraNFT.accruePoints(account, quantity);
             }
         }
     }
