e5f28071dbdf196aa8df4a514615b2270c8f7acc
Topdevbeast <topdevbeast@gmail.com>
2022-01-20 19:09:58 -0800

    update function header

--- contracts/interfaces/IAuraNFT.sol
+++ contracts/interfaces/IAuraNFT.sol
@@ -2,7 +2,7 @@
 pragma solidity >= 0.8.0;
 
 interface IAuraNFT {
-    function accruePoints(address user, uint amount) external;
+    function accrueAuraPoints(address user, uint amount) external;
     function setIsStaked(uint tokenId, bool isStaked) external;
     function getAuraPoints(uint tokenId) external view returns(uint);
     function getInfoForStaking(uint tokenId) external view returns(address tokenOwner, bool isStaked, uint auraPoints);
