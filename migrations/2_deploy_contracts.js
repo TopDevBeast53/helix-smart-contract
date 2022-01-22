8b33e4bd6f06f1c2180b18afa6a67da29bd5405b
Topdevbeast <topdevbeast@gmail.com>
2022-01-21 21:40:41 -0800

    add contract deployments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,44 +11,56 @@ const SwapFeeRewardsWithAP = artifacts.require('SwapFeeRewardsWithAP');
  *       isolated for initial development and not intended to be used for production.
  */
 
-/*
- * NOTE: The following constants are intended to be used only for initial SwapFeeRewardsWithAP
- *       deployment. They're not addresses pointing to deployed contracts so, SwapFeeRewardWithAP 
- *       functions that rely on these contracts are not expected to work. 
- */
 const FACTORY = {
-    'test': '0x859f1D503E588f33ed69858B53b4C25647D2e5dE',
+    'test': '0xe1cf8d44bb47b8915a70ea494254164f19b7080d',
 };
 
 const ROUTER = {
-    'test': '0xB8FB1967B73b0D05D73095a6c0CFbaB715a1268f',
-};
-
-const TARGET_TOKEN = {
-    'test': '0x38d628e5898E8420b807E8653d6CB0dBc0Cd1294',
+    'test': '0x38433227c7a606ebb9ccb0acfcd7504224659b74',
 };
 
 const TARGET_AP_TOKEN = {
-    'test': '0x92e92FC751ca101f8309DdB753230aEa921F5cb2',
+    'test': '0x9903Ee9e2a67D82A2Ba37D087CC8663F9592716E',
 };
 
+const AURA_NFT = {
+    'test': '0x746eD187F197f6a562ffFc0285251420E55A8060',
+};
+
+const AURA_TOKEN = {
+    'test': '0xdf2b1082eE98b48B5933378c8F58cE2f5AaFF135',
+};
+
+const SWAP_FEE = {
+    'test': '0x3427F78a570605003531547fC444ce4f521ac5B3',
+};
+
+// NOTE: This is a mock address and doesn't point at a deployed contract.
 const ORACLE = {
     'test': '0xd099dF8fD5eEdBe00d88a41503FDcD89FD10E1f4',
 };
 
-const AURA_NFT = {
-    'test': '0x2A3c379E6922b464187200AaEB8853caac63fF13',
-};
+async function deployAuraNFT(deployer) {
+    await deployer.deploy(AuraNFT, 
+        "",         // string memory baseURI
+        10000,      // uint initialAuraPoints
+        10,         // uint8 levelUpPercent
+    )
+}
 
-const AURA_TOKEN = {
-    'test': '0x9dC08b715eaE6F9E416FF21067D3D9e21490e3DF',
-};
+async function deployAuraToken(deployer) {
+    await deployer.deploy(AuraToken);
+}
+
+async function deployTargetAPToken(deployer) {
+    await deployer.deploy(AuraLP); 
+}
 
 async function deploySwapFeeRewardsWithAP(deployer, env) {
     await deployer.deploy(SwapFeeRewardsWithAP, 
         FACTORY[env],
         ROUTER[env],
-        TARGET_TOKEN[env],
+        AURA_TOKEN[env],        // targetToken == auraToken.address
         TARGET_AP_TOKEN[env],
         ORACLE[env],
         AURA_NFT[env],
@@ -57,5 +69,8 @@ async function deploySwapFeeRewardsWithAP(deployer, env) {
 };
 
 module.exports = async function (deployer) {
-    deploySwapFeeRewardsWithAP(deployer, 'test');
+    //deployAuraNFT(deployer);
+    //deployAuraToken(deployer);
+    //deployTargetAPToken(deployer);
+    //deploySwapFeeRewardsWithAP(deployer, 'test');
 };
