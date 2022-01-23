3d5ecf429e3a6421ba5e5dea05d9b40e2653f3d0
Topdevbeast <topdevbeast@gmail.com>
2022-01-22 16:00:38 -0800

    load contract

--- scripts/interactSwapFeeRewardsWithAP.js
+++ scripts/interactSwapFeeRewardsWithAP.js
@@ -1,11 +1,20 @@
 /* 
- * TODO
+ * @dev Interaction script for deployed Swap Fee Rewards with AP contract.
+ * 
+ * Run from project root using:
+ *     truffle exec scripts/interactSwapFeeRewardsWithAP.js --network bsc_testnet 
  */
 
-const Web3 = require('web3');
-
 async function main() {
-    console.log("WEB3", Web3);
+    const Web3 = require('web3');
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545'));
+    
+    let abi = require('../build/contracts/SwapFeeRewardsWithAP.json').abi;
+    let address = '0x3427F78a570605003531547fC444ce4f521ac5B3';
+    let swapFeeRewardsWithAP = new web3.eth.Contract(abi, address);
+
+    // Expect the following to return 0.
+    console.log("Get pairs list length: ", await swapFeeRewardsWithAP.methods.getPairsListLength().call());
 };
 
 main()
