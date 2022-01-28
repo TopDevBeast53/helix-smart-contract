685a14a8b841a2ad1430a03cd2d03dc1e2d96a43
Topdevbeast <topdevbeast@gmail.com>
2022-01-27 19:03:55 -0800

    set the dependent contracts

--- scripts/interactSwapFee.js
+++ scripts/interactSwapFee.js
@@ -2,7 +2,7 @@
  * @dev Interaction script for deployed Swap Fee Rewards with AP contract.
  * 
  * Run from project root using:
- *     npx hardhat run scripts/interactSwapFeeRewardsWithAP.js --network testnetBSC
+ *     npx hardhat run scripts/interactSwapFee.js --network testnetBSC
  */
 
 require ('dotenv').config();
@@ -14,7 +14,8 @@ let owner, user;
 let router, market, auction;
 let tokenA, tokenB;
 let ISwapFee, swapFee;
-let tx;
+let tx, nonce;
+let gasLimit;
 
 /**
  * @dev Initialize the contract and call functions.
@@ -24,14 +25,14 @@ async function main() {
     await init(); 
    
     // Prepare the token pair for swapping.
-    //await preparePair(tokenA, tokenB); 
+    await preparePair(tokenA, tokenB); 
    
     // Swap the tokens.
     let account = await user.getAddress();
     tokenA = Address.AuraNFT;
     tokenB = Address.BnbToken;
     let amount = 100;
-    await swap(account, tokenA, tokenB, amount);
+    //await swap(account, tokenA, tokenB, amount);
 
     //await accrueAPFromMarket(account, tokenA, amount);
 
@@ -47,14 +48,15 @@ async function init() {
     Address = {
         Owner: '0x59201fb8cb2D61118B280c8542127331DD141654',
         User: '0x697419d2B31844ad7Fa4646499f8B81de79D2eB1',
-        SwapFee: '0x380D2a5Cc9E5e980EdeC79bD5bee9C7c0c8E50da',      // Deployed
-        Factory: '0xe1cf8d44bb47b8915a70ea494254164f19b7080d',      // Deployed
-        Router: '0x38433227c7a606ebb9ccb0acfcd7504224659b74',       // Deployed
-        AuraToken: '0xdf2b1082ee98b48b5933378c8f58ce2f5aaff135',    // Deployed 
-        BnbToken: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',     // Deployed
-        AuraNFT: '0x6f567929bac6e7db604795fC2b4756Cc27C0e020',      // Deployed
-        Market: '0xB69888c53b9c4b779E1bEAd3A5019a388Bc072e9',       // Fake
-        Auction: '0xdCe96794ba50b147C60F35D614e76451062fBce7',      // Fake
+        SwapFee: '0xAd34Cac48cAC8e8dD0b46134f796F983ACd10bb6',          // Deployed
+        Factory: '0xe1cf8d44bb47b8915a70ea494254164f19b7080d',          // Deployed
+        Router: '0x38433227c7a606ebb9ccb0acfcd7504224659b74',           // Deployed
+        AuraToken: '0xdf2b1082ee98b48b5933378c8f58ce2f5aaff135',        // Deployed - Also used for TargetToken
+        BnbToken: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',         // Deployed
+        AuraNFT: '0x6f567929bac6e7db604795fC2b4756Cc27C0e020',          // Deployed
+        TargetAPToken: '0x9903Ee9e2a67D82A2Ba37D087CC8663F9592716E',    // Deployed
+        Market: '0xB69888c53b9c4b779E1bEAd3A5019a388Bc072e9',           // Fake
+        Auction: '0xdCe96794ba50b147C60F35D614e76451062fBce7',          // Fake
     }
 
     // Load the provider.
@@ -75,14 +77,127 @@ async function init() {
     const swapFeeAbi = swapFeeJson.abi;
     ISwapFee = await ethers.getContractFactory('SwapFeeRewardsWithAP');
 
-    // Define the transaction parameters for the owner.
-    overrides = {
-        gasLimit: 6721975,
-    };
+    gasLimit = 6721975;
 
     // Set the tokens to test.
     tokenA = Address.AuraToken;
     tokenB = Address.BnbToken;
+
+    // Set the contract's dependents.
+    swapFee = await ISwapFee.attach(Address.SwapFee).connect(owner);
+    nonce = await network.provider.send('eth_getTransactionCount', [Address.Owner, "latest"]);
+
+    // Set the factory.
+    let factoryAddress = await swapFee.factory();
+    if (factoryAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set factory address to ${Address.Factory}`);
+        }
+        tx = await swapFee.setFactory(Address.Factory);
+        await tx.wait();
+    }
+    factoryAddress = await swapFee.factory();
+    if (verbose) {
+        console.log(`Factory address is ${factoryAddress}`);
+    } 
+  
+    // Set the router.
+    let routerAddress = await swapFee.router();
+    if (routerAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set router address to ${Address.Router}`);
+        }
+        tx = await swapFee.setRouter(Address.Router);
+        await tx.wait();
+    }
+    routerAddress = await swapFee.router();
+    if (verbose) {
+        console.log(`Router address is ${routerAddress}`);
+    } 
+
+    // Set the target token
+    let targetTokenAddress = await swapFee.targetToken();
+    if (targetTokenAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set target token address to ${Address.AuraToken}`);
+        }
+        tx = await swapFee.setTargetToken(Address.AuraToken);
+        await tx.wait();
+    }
+    targetTokenAddress = await swapFee.targetToken();
+    if (verbose) {
+        console.log(`Target token address is ${targetTokenAddress}`);
+    } 
+
+    // Set the target AP token
+    let targetAPTokenAddress = await swapFee.targetAPToken();
+        if (targetAPTokenAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set target AP token address to ${Address.TargetAPToken}`);
+        }
+        tx = await swapFee.setTargetAPToken(Address.TargetAPToken);
+        await tx.wait();
+    }
+    targetAPTokenAddress = await swapFee.targetAPToken();
+    if (verbose) {
+        console.log(`Target AP token address is ${targetAPTokenAddress}`);
+    } 
+
+    // Set the aura token
+    let auraTokenAddress = await swapFee.auraToken();
+    if (auraTokenAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set aura token address to ${Address.AuraToken}`);
+        }
+        tx = await swapFee.setAuraToken(Address.AuraToken);
+        await tx.wait();
+    }
+    auraTokenAddress = await swapFee.auraToken();
+    if (verbose) {
+        console.log(`Aura token address is ${auraTokenAddress}`);
+    } 
+
+    // Set the aura NFT
+    let auraNFTAddress = await swapFee.auraNFT();
+    if (auraNFTAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set aura NFT address to ${Address.AuraNFT}`);
+        }
+        tx = await swapFee.setAuraNFT(Address.AuraNFT);
+        await tx.wait();
+    }
+    auraNFTAddress = await swapFee.auraNFT();
+    if (verbose) {
+        console.log(`Aura NFT address is ${auraNFTAddress}`);
+    } 
+
+    // Set the market
+    let marketAddress = await swapFee.market();
+    if (marketAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set market address to ${Address.Market}`);
+        }
+        tx = await swapFee.setMarket(Address.Market);
+        await tx.wait();
+    }
+    marketAddress = await swapFee.market();
+    if (verbose) {
+        console.log(`Market address is ${marketAddress}`);
+    } 
+
+    // Set the auction
+    let auctionAddress = await swapFee.auction();
+    if (auctionAddress == ethers.constants.AddressZero) {
+        if (verbose) {
+            console.log(`Set auction address to ${Address.Auction}`);
+        }
+        tx = await swapFee.setAuction(Address.Auction);
+        await tx.wait();
+    }
+    auctionAddress = await swapFee.auction();
+    if (verbose) {
+        console.log(`Auction address is ${auctionAddress}`);
+    } 
 }
 
 /**
@@ -132,7 +247,7 @@ async function getPairExists(tokenA, tokenB) {
  */
 async function addPair(tokenA, tokenB, percentReward) {
     const pairAddress = await swapFee.pairFor(tokenA, tokenB)
-    const addPairTx = await swapFee.addPair(percentReward, pairAddress, overrides);
+    const addPairTx = await swapFee.addPair(percentReward, pairAddress);
 
     if (verbose) {
         console.log(`(${short(tokenA)}, ${short(tokenB)}) pair address: ${pairAddress}`);
