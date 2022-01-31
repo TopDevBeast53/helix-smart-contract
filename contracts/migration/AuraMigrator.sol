9b9b047b9a2f3047e1c4b6cc9e1bfb70730cc8c6
Topdevbeast <topdevbeast@gmail.com>
2022-01-30 22:07:40 -0800

    refactor migrator

--- contracts/migration/AuraMigrator.sol
+++ contracts/migration/AuraMigrator.sol
@@ -4,63 +4,42 @@ pragma solidity >=0.8.0;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '../interfaces/IAuraMigrator.sol';
-import '../swaps/AuraFactory.sol';
 import '../interfaces/IAuraV2Router02.sol';
 
 contract AuraMigrator is IAuraMigrator, Ownable {
-    AuraFactory public factory; 
-    IAuraV2Router02 public router;
-
-    modifier addressNotZero(address _address) {
-        require(_address != address(0), 'AuraMigrator: Address is Zero');
-        _;
-    }
-
-    constructor(address _factory, address _router) {
-        setFactory(_factory);
+    constructor(address _router) {
         setRouter(_router);
     }
 
     /** 
-     * @dev Migrate liquidity from external DEX to this DEX.
+     * @dev Migrate liquidity pair (tokenA, tokeB) from external DEX to this DEX.
      */
     function migrateLiquidity(address tokenA, address tokenB, address lpToken, address externalRouter) external {
-        // Transfer the caller's LP balance to this contract.
-        uint amount = IERC20(lpToken).balanceOf(msg.sender);
-        IERC20(lpToken).transferFrom(msg.sender, address(this), amount);
+        // Transfer the caller's liquidity balance to this contract.
+        uint liquidity = IERC20(lpToken).balanceOf(msg.sender);
+        IERC20(lpToken).transferFrom(msg.sender, address(this), liquidity);
 
-        uint prevBalanceTokenA = IERC20(tokenA).balanceOf(address(this));
-        uint prevBalanceTokenB = IERC20(tokenB).balanceOf(address(this));
+        // Approve external router to spend up to `liquidity` amount of the liquidity.
+        IERC20(lpToken).approve(externalRouter, liquidity);
 
-        // Approve removing amount of lpToken on caller's behalf. 
-        IERC20(lpToken).approve(externalRouter, amount);
-
-        // And remove the liquidity from the external router.
-        IAuraV2Router02(externalRouter).removeLiquidity(
+        // Remove the token balances from the external exchange.
+        (uint exBalanceTokenA, uint exBalanceTokenB) = IAuraV2Router02(externalRouter).removeLiquidity(
             tokenA,             // address of tokenA
             tokenB,             // address of tokenB
-            amount,             // liquidity amount to remove
+            liquidity,          // amount of liquidity to remove
             0,                  // minimum amount of A
             0,                  // minimum amount of B
             address(this),      // recipient of underlying assets
             block.timestamp     // deadline until tx revert
         );
 
-        uint balanceTokenA = IERC20(tokenA).balanceOf(address(this)) - prevBalanceTokenA;
-        uint balanceTokenB = IERC20(tokenB).balanceOf(address(this)) - prevBalanceTokenB;
+        // Approve this router to spend up to the external token balances.
+        IERC20(tokenA).approve(address(router), exBalanceTokenA);
+        IERC20(tokenB).approve(address(router), exBalanceTokenB);
 
-        // Add this pair to the factory if it's not already there.
-        address auraPair = factory.getPair(tokenA, tokenB);
-        if (auraPair == address(0)) {
-            factory.createPair(tokenA, tokenB);
-        }
-
-        // Approve adding tokens A and B on caller's behalf.
-        IERC20(tokenA).approve(address(router), balanceTokenA);
-        IERC20(tokenB).approve(address(router), balanceTokenB);
-
-        // And add the tokens to aura router.
-        router.addLiquidity(
+        // Add the external token balances to this exchange.
+        // Note: addLiquidity handles adding token pair to factory.
+        (uint balanceTokenA, uint balanceTokenB, uint liquidity) = router.addLiquidity(
             tokenA,             // address of token A
             tokenB,             // address of token B
             balanceTokenA,      // desired amount of A
@@ -70,19 +49,26 @@ contract AuraMigrator is IAuraMigrator, Ownable {
             msg.sender,         // liquidity tokens recipient
             block.timestamp     // deadline until tx revert
         ); 
+
+        // Return any dust to the caller.
+        if (exBalanceTokenA > balanceTokenA) {
+            uint dustTokenA = exBalanceTokenA - balanceTokenA;
+            IERC20(tokenA).transfer(msg.sender, dustTokenA);
+        }
+        if (exBalanceTokenB > balanceTokenB) {
+            uint dustTokenB = exBalanceTokenB - balanceTokenB;
+            IERC20(tokenB).transfer(msg.sender, dustTokenB);
+        }
+        if (liquidity > 0) {
+            IERC20(liquidity).transfer(msg.sender, liquidity);
+        }
     }
 
     /**
-     * @dev Changes the factory address.
-     */
-    function setFactory(address _factory) public onlyOwner addressNotZero(_factory) {
-        factory = AuraFactory(_factory); 
-    }
-
-    /**
-     * @dev Changes the router address.
+     * @dev Sets the router address.
      */
     function setRouter(address _router) public onlyOwner addressNotZero(_router) {
+        require(_address != address(0), 'AuraMigrator: Router address is Zero');
         router = IAuraV2Router02(_router);
     }
 }
