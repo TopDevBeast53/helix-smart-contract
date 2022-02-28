16d1196cde2c65c3a2c9bec6245e7ba4e4e63271
Topdevbeast <topdevbeast@gmail.com>
2022-02-27 21:37:49 -0800

    clean up interact swap fee contract address file

--- scripts/constants/interactSwapFeeContracts.js
+++ scripts/constants/interactSwapFeeContracts.js
@@ -1,8 +1,11 @@
+// This file is used by the interactSwapFee script for loading the addresses of it's
+// dependencies. Although the previously deployed contracts didn't work for the
+// swapFee interaction script that doesn't mean they must be replaced. Hence, a
+// separate file for the interactSwapFee script.
 module.exports = {
     auraToken: {
         'main': '',
-        //'test': '0xdf2b1082eE98b48B5933378c8F58cE2f5AaFF135',
-        'test': '0xE75cca98b0274b7A6c1C6728870497eE1b3170e7',       // use Test Token D instead since transfer BEP20 to AuraPair fails
+        'test': '0xE75cca98b0274b7A6c1C6728870497eE1b3170e7',       // use Test Token D instead since transferring BEP20 to AuraPair fails
     },
     auraNFT: {
         'main': '',
@@ -10,8 +13,7 @@ module.exports = {
     },
     auraLP: {
         'main': '',
-        //'test': '0x529cce4dd9c2234be9ca724a4158b8f675b22778',     // Doesn't transfer to AuraPair
-        'test': '0x0B8c4BBC3C63Cc96AF95e250e1B83E9Da7c9830F',       // using Test Token C instead for now
+        'test': '0x0B8c4BBC3C63Cc96AF95e250e1B83E9Da7c9830F',       // using Test Token C instead since transfer to AuraPair fails
     },
     auraNFTImpl: {
         'main': '',
@@ -27,8 +29,7 @@ module.exports = {
     },
     router: {
         'main': '',
-        //'test': '0x38433227c7a606ebb9ccb0acfcd7504224659b74',
-        'test': '0xA12933bf8816fc65448A369f33f78F3f0a7Df24D',       // Deployed by @iirikr on Feb 27
+        'test': '0xA12933bf8816fc65448A369f33f78F3f0a7Df24D',       // Redeployed because previous deployment was failing
     },
     WBNB: {
         'main': '',
@@ -36,18 +37,15 @@ module.exports = {
     },
     factory: {
         'main': '',
-        //'test': '0x84f22547020f582Deef1eb1B57b3b213D5997471',
-        'test': '0x361376063722277DbD75Cb73aFe2652589AbDDd8',       // Deployed by @iirikr on Feb 27
+        'test': '0x361376063722277DbD75Cb73aFe2652589AbDDd8',       // Replaces previous factory which didn't have dev team wallet address as feeToSetter
     },
     oracle: {
         'main': '',
-        //'test': '0xFC7EF377E976cC45c1D647d7060a8e4C1Fa26e4c',
-        'test': '0x1322Ea12fd00325181675DfdC68159991AA798A4',       // Deployed by @iirikr on Feb 27 w/ window=400 and granularity=2 for faster execution
+        'test': '0x1322Ea12fd00325181675DfdC68159991AA798A4',       // Uses window=400 and granularity=2 for successful (and faster) interaction. 
     },
     swapFee: {
         'main': '',
-        //'test': '0x178AA574944B3F3c4c89A1Ed4448F157aC041aFE',
-        'test': '0x4835Cc23Fe71d50d3753F48CaCeeC74369585230',       // Deployed by @iirikr on Feb 27
+        'test': '0x4835Cc23Fe71d50d3753F48CaCeeC74369585230',       // Redeployed because previous deployment was failing
     },
     testTokenA: {
         'main': '',
