678b6c619d7b6c3b14dac8ed2503db41f150ba0d
Topdevbeast <topdevbeast@gmail.com>
2022-03-13 16:40:49 -0400

    make transfer_in

--- solana_anchor/programs/solana_anchor/src/lib.rs
+++ solana_anchor/programs/solana_anchor/src/lib.rs
@@ -1,15 +1,45 @@
 use anchor_lang::prelude::*;
-
+use anchor_spl::token::{self, Token, Transfer};
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod solana_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn transfer_in(ctx: Context<TransferIn>) -> Result<()> {
+        {
+            let cpi_ctx = CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.user.to_account_info(),
+                    to: ctx.accounts.pool_signer.to_account_info(),
+                    authority: ctx.accounts.user.to_account_info(),
+                },
+            );
+            token::transfer(cpi_ctx, 1)?;
+        }
+        Ok(())
+    }
+
+    pub fn transfer_out(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferIn<'info> {
+    pool_signer: UncheckedAccount<'info>,
+    user: Signer<'info>,
+    token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize {
+
+}
+
+#[error]
+pub enum CustomeError {
+    #[msg("Owner can transfer the token")]
+    IsNotOwner
+}
