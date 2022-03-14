9281fd5ca48fbe7c7032b05828832153332e3c0b
Topdevbeast <topdevbeast@gmail.com>
2022-03-14 11:16:10 -0700

    Update contracts.js

--- scripts/constants/contracts.js
+++ scripts/constants/contracts.js
@@ -1,23 +1,23 @@
 module.exports = {
     factory: {
         'main': '',
-        'test': '0xa36dd84a81696C46E70Ae4Cc6be3682c8E65A6DE',
+        'test': '',
     },
         router: {
         'main': '',
-        'test': '0xF5149C14Fc8834fdFc27ecd4b17736b75cF6B6aF',
+        'test': '',
     },
     auraToken: {
         'main': '',
-        'test': '0xdf2b1082eE98b48B5933378c8F58cE2f5AaFF135',
+        'test': '',
     },
     oracleFactory: {
         'main': '',
-        'test': '0x1219e8129A09D3d6Ae4976c93F0764127f39BA19',
+        'test': '',
     },
     referralRegister: {
         'main': '',
-        'test': '0xE6d9e06637b456dcaACe6EDc0A8a9a7443078825',
+        'test': '',
     },
     auraLP: {
         'main': '',
@@ -37,7 +37,7 @@ module.exports = {
     },
     auraNFT: {
         'main': '',
-        'test': '0x8b2b085339D11DcF0FA0aaD985C30f82eb49a880',
+        'test': '',
     },
     auraNFTImpl: {
         'main': '',
@@ -51,13 +51,9 @@ module.exports = {
         'main': '',
         'test': '',
     },
-    apToken: {
+    swapRewards: {
         'main': '',
-        'test': '0x0B8c4BBC3C63Cc96AF95e250e1B83E9Da7c9830F',
-    },
-    swapFee: {
-        'main': '',
-        'test': '0xf05f26059bF589347FBf532D7009e8Cb27922e68',
+        'test': '',
     },
     auraMigrator: {
         'main': '',
