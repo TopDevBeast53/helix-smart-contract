3d3fa58271ab73464f639463305a993aa11ce6f9
Topdevbeast <topdevbeast@gmail.com>
2022-03-23 00:51:47 -0700

    reformat code

--- solana_anchor/client/main.js
+++ solana_anchor/client/main.js
@@ -1,21 +1,16 @@
-const { 
-    Connection, 
-    PublicKey, 
-    clusterApiUrl, 
-  } = require('@solana/web3.js');
-const TOKEN_PROGRAM_ID = require('@solana/spl-token');
+const { Connection, PublicKey, clusterApiUrl } = require("@solana/web3.js");
+const TOKEN_PROGRAM_ID = require("@solana/spl-token");
+const { Program, Provider, Wallet } = require("@project-serum/anchor");
+const Contract = require("web3-eth-contract");
+const dotenv = require("dotenv").config();
 const {
-  Program,
-  Provider,
-  Wallet
-} = require('@project-serum/anchor') 
-const Contract = require('web3-eth-contract');
-const dotenv = require('dotenv');
-const { getOrCreateAssociatedTokenAccount, createKeypairFromFile } = require('./util.js'); 
-const idl = require('../target/idl/solana_anchor.json');
+  getOrCreateAssociatedTokenAccount,
+  createKeypairFromFile,
+} = require("./util.js");
+const idl = require("../target/idl/solana_anchor.json");
 
 async function main() {
-  const network = 'devnet';
+  const network = dotenv.parsed.NETWORK;
   const endpoint = () => clusterApiUrl(network);
   const opts = {
     preflightCommitment: "recent",
@@ -25,23 +20,24 @@ async function main() {
     commitment: "processed",
   };
 
-  console.debug('info', dotenv)
   const getClusterURL = endpoint;
-  const programID = new PublicKey("A7nCafiWF1mDUHYJxfXGaBX3vJm7XvzkUtgSe9R1kK9D");
-  
+  const programID = new PublicKey(dotenv.parsed.PROGRAM_ID);
+
   const connection = new Connection(getClusterURL(), opts.preflightCommitment);
-  
-  const keyPair = await createKeypairFromFile('target/deploy/solana_anchor-keypair.json');
+
+  const keyPair = await createKeypairFromFile(
+    "target/deploy/solana_anchor-keypair.json"
+  );
   const wallet = new Wallet(keyPair);
-  
+
   const provider = new Provider(connection, wallet, defaultOpts);
   const program = new Program(idl, programID, provider);
 
-  const contract = new Contract()
-  
+  const contract = new Contract();
+
   // const senderATA = await getOrCreateAssociatedTokenAccount(wallet.publicKey, mint, wallet.publicKey, wallet.signTransaction)
   // const receiverATA = await getOrCreateAssociatedTokenAccount(wallet.publicKey, mint, programID, wallet.signTransaction);
-  
+
   // await program.rpc.transferOut(
   //     uint8array,
   //     {
@@ -58,4 +54,3 @@ async function main() {
 }
 
 main();
-
