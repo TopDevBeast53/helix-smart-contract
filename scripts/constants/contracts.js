3bedad63c4511779d8238159b7febe67e0059691
Topdevbeast <topdevbeast@gmail.com>
2022-03-25 16:03:30 -0700

    redeploy full exchange

--- scripts/constants/contracts.js
+++ scripts/constants/contracts.js
@@ -1,67 +1,67 @@
 module.exports = {
     factory: {
         56: '',
-        97: '0x4B6c7886B0BF73dF32eCDAe3D0Df10A1C329c226',
+        97: '0x5afc67B783F0B40d06d3Ba4FB3fEe0E26ed33B28',
     },
     router: {
         56: '',
-        97: '0x9d1f837c02d9aD57C6952984DA17c3B80bFa63A8',
+        97: '0xC6C81928295B36E2Cd3cb59a806E19aba0A2b039',
     },
     auraToken: {
         56: '',
-        97: '0xFA9ac73063a4b81CDae02c34c6218a70164D4A3e',
+        97: '0xD3281f47f12Ad21027C494A643256151c6FC9b56',
     },
     oracleFactory: {
         56: '',
-        97: '0xF75a0D482F9f205B6c96eCbD4B61f8771085e6C2',
+        97: '0xd7f1FE06687D542161BFC89515b51c26DfF8FFa5',
     },
     referralRegister: {
         56: '',
-        97: '0x4c91735899D3736eD45730b4F6Cd8625f9dde19B',
+        97: '0xa30F620f8c2f342F5c0A72Ed8E318F3B2b376ba5',
     },
     masterChef: {
         56: '',
-        97: '0xd06cA59a8d280C3a1AA2B0Cb1256796337F0A06A',
+        97: '0xf27D4645E017Cf0895CFFF520F9008F792070564',
     },
     autoAura: {
         56: '',
-        97: '0x746AEC6c33A3c50D67ba7014Cd5b91B181421EE5',
+        97: '0x75739a7cDF104Ee8c33F3913FAf5cD123Cb04D69',
     },
     smartChef: {
         56: '',
-        97: '0xcAe8A6c8d91C9c635D953E4faC63438865f5d08a',
+        97: '0x0764bA69073f6C3887CE4f47335034E4bE7D880a',
     },
     auraNFT: {
         56: '',
-        97: '0x94DF97954ec548bd2Db1c9357982445A7245363d',
+        97: '0x0c753D2f5F6D68D3024C98db6B791B3845CAF274',
     },
     auraNFTChef: {
         56: '',
-        97: '0x6cc6f1F6eD0d2A13635034Cf2A0E2934449d2C38',
+        97: '0xA25ff5BDDB9970457bBF0AdA3BF5Dc7ed4977751',
     },
     auraNFTBridge: {
         56: '',
-        97: '0x4F807c0f58e88A2fEee50bC9dd80db0923e46002',
+        97: '0xb9a6799A2e5Fceae3Fa3cff022a41f54eDF61A62',
     },
     apToken: {
         56: '',
-        97: '0x5Ff8cD8a2766B544EcCa532E545a22FE433EaF5b',
+        97: '0x69F6ce62d0e6Ffcf85556F06adEcC16722b3D311',
     },
     swapRewards: {
         56: '',
-        97: '0x487c7c433283bcC5410d002bF557D4D4D4D86beA',
+        97: '0x28Adc349661318a8Cc146ccde82046Fb856De9b5',
     },
     auraMigrator: {
         56: '',
-        97: '0xB5bd13566919D856D2d2A3e5E2908b95390118dF',
+        97: '0x5E486eA7AccF2D8e725bAd985Eb1D69a6465791F',
     },
     tokenTools: {
         56: '',
-        97: '0xdf10C205011bb36fda556d9C1521bCeAe0f32551',
+        97: '0x1c10F01ceff85715Fc2177A3BC3b70D11503E548',
     },
     auraVault: {
         56: '',
-        97: '0xf2fc3f8F3A0630C82f6d0837B713c186f9748f2e',
+        97: '',
     },
     auraNFTImpl: {
         56: '',
