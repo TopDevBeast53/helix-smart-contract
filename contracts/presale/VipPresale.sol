38ffa1f82a1f25be5f8963ad78b2f3554da96361
Topdevbeast <topdevbeast@gmail.com>
2022-03-29 10:43:50 -0700

    clean up phases/subPhases settings

--- contracts/presale/VipPresale.sol
+++ contracts/presale/VipPresale.sol
@@ -9,33 +9,36 @@ contract VipPresale is Ownable {
      * Phase 0: is the default on contract creation
      *          purchases are prohibited
      *          sales are prohibited
-     * Phase 1: is set by the owner
+     * Phase 1: manually set by the owner, starts phase sequence
      *          purchases are limited by a user's `maxTickets` 
      *          sales are prohibited
      * Phase 2: is automatically set 24 hours after the start of Phase 1
      *          purchases are unlimited
      *          sales are limited by subPhase 
      */
-    uint public MAX_PHASE;
-    uint public phase;
-    uint public phaseStartTimestamp;
-    uint public PHASE_DURATION;
+    uint public MAX_PHASE;              // Highest phase attainable
+    uint public START_PHASE;            // Phase which starts automatic sequence
+    uint public phase;                  // Current phase
+    uint public phaseEndTimestamp;      // End timestamp after which the current phase has ended
+    uint public PHASE_DURATION;         // Length of time for a phase
     
     /* 
      * SubPhase determines ticket sales by whitelisted users during Phase 2
-     * SubPhase 0: sales are prohibited
-     * SubPhase 1: users may sell up to 25% of their purchased tickets
-     * SubPhase 2: users may sell up to 50% of their purchased tickets
-     * SubPhase 3: users may sell up to 75% of their purchased tickets
-     * SubPhase 4: users may sell up to 100% of their purchased tickets
+     * SubPhase 0: default on contract creation, nothing happens
+     * SubPhase 1: started manually by the owner, starts subPhase sequence
+     * SubPhase 2: users may sell up to 25% of their purchased tickets
+     * SubPhase 3: users may sell up to 50% of their purchased tickets
+     * SubPhase 4: users may sell up to 75% of their purchased tickets
+     * SubPhase 5: users may sell up to 100% of their purchased tickets
      * 
-     * SubPhase 0 begins simultaneously with the start of Phase 2
-     * Subsequent subPhases begin 3 months after the start of the previous subPhase
+     * After subPhase 1, subsequent subPhases automatically begin `SUB_PHASE_DURATION` 
+     * after the start of the previous subPhase
      */
-    uint public MAX_SUB_PHASE;
-    uint public subPhase;
-    uint public subPhaseStartTimestamp;
-    uint public SUB_PHASE_DURATION;
+    uint public MAX_SUB_PHASE;          // Highest subPhase attainable
+    uint public START_SUB_PHASE;        // SubPhase which starts automatic sequence
+    uint public subPhase;               // Current subPhase
+    uint public subPhaseEndTimestamp;   // End timestamp after which the current subPhase has ended
+    uint public SUB_PHASE_DURATION;     // Lenght of time for a subPhase
   
     // Maximum number of tickets available for purchase at the start of the sale
     uint public MAX_TICKETS;
@@ -61,8 +64,8 @@ contract VipPresale is Ownable {
     // true if user can purchase tickets and false otherwise
     mapping(address => bool) public whitelist;
 
-    event SetPhase(uint phase, uint timestamp);
-    event SetSubPhase(uint subPhase, uint timestamp);
+    event SetPhase(uint phase, uint startTimestamp, uint endTimestamp);
+    event SetSubPhase(uint subPhase, uint startTimestamp, uint endTimestamp);
 
     modifier isValidPhase(uint _phase) {
         require(_phase <= MAX_PHASE, "VipPresale: PHASE CAN'T BE GREATER THAN MAX_PHASE");
@@ -86,53 +89,58 @@ contract VipPresale is Ownable {
 
     constructor() {
         MAX_PHASE = 2;
-        MAX_SUB_PHASE = 4;
-        MAX_TICKETS = 50000;
+        START_PHASE = 1;
         PHASE_DURATION = 1 days;
+
+        MAX_SUB_PHASE = 5;
+        START_SUB_PHASE = 1;
         SUB_PHASE_DURATION = 91 days;   // (91 days ~= 3 months) and (91 days * 4 ~= 1 year)
+
+        MAX_TICKETS = 50000;
     }
 
     // called periodically and, if sufficient time has elapsed, update the phase
     function updatePhase() private {
-        if (phase < MAX_PHASE && block.timestamp >= phaseStartTimestamp + PHASE_DURATION) {
-            _setPhase(phase + 1);
+        if (phase >= START_PHASE) {
+            if (phase < MAX_PHASE && block.timestamp >= phaseEndTimestamp) {
+                _setPhase(phase + 1);
+            }
         }
     }
 
     // used externally to update from phase 0 to phase 1
-    // and should only ever be called to set phase == 1
+    // should only ever be called to set phase == 1
     function setPhase(uint _phase) external onlyOwner isValidPhase(_phase) {
         _setPhase(_phase);
-        _setSubPhase(0);    // extend functionality to more than 2 phases, each with their own subPhases
     }
 
-    // used internally to update from phase 1 to phase 2
+    // used internally to update phases
     function _setPhase(uint _phase) private {
         phase = _phase;
-        phaseStartTimestamp = block.timestamp;
-        emit SetPhase(_phase, phaseStartTimestamp);
+        phaseEndTimestamp = block.timestamp + PHASE_DURATION;
+        emit SetPhase(_phase, block.timestamp, phaseEndTimestamp);
     }
 
     // called periodically and, if sufficient time has elapsed, update the subPhase
     function updateSubPhase() private {
-        if (phase > 1) {
-            if (subPhase < MAX_SUB_PHASE && block.timestamp >= subPhaseStartTimestamp + SUB_PHASE_DURATION) {
+        if (subPhase >= START_SUB_PHASE) {
+            if (subPhase < MAX_SUB_PHASE && block.timestamp >= subPhaseEndTimestamp) {
                 _setSubPhase(subPhase + 1);
             }
         }
     }
 
-    // should never be needed but exists to provide functionality for
-    // manually setting the subphase
+    // used externally to update from subPhase 0 to subPhase 1
+    // should only ever be called to set subPhase == 1
     function setSubPhase(uint _subPhase) external onlyOwner isValidSubPhase(_subPhase) {
         _setSubPhase(_subPhase);
     }
 
-    // used internally during phase 2 to update sub-phases
+    // used internally to update subPhases
     function _setSubPhase(uint _subPhase) private {
         subPhase = _subPhase;
-        subPhaseStartTimestamp = block.timestamp;
-        emit SetSubPhase(_subPhase, subPhaseStartTimestamp);
+        subPhaseEndTimestamp = block.timestamp + SUB_PHASE_DURATION;
+        emit SetSubPhase(_subPhase, block.timestamp, subPhaseEndTimestamp);
     }
 
     // grant `user` permission to purchase up to `maxTickets`, phase dependent
