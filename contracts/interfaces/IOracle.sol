d521faa0157f027a93ac235255c4161e856a81c5
Topdevbeast <topdevbeast@gmail.com>
2022-05-03 13:02:41 -0700

    replace single with double quote and uint with uint256

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -2,6 +2,6 @@
 pragma solidity >=0.8.0;
 
 interface IOracle {
-    function consult(address tokenIn, uint amountIn) external view returns (uint amountOut);
+    function consult(address tokenIn, uint256 amountIn) external view returns (uint256 amountOut);
     function update() external;
 }
--- contracts/interfaces/ISwapRewards.sol
+++ contracts/interfaces/ISwapRewards.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.8.0;
 
 interface ISwapRewards {
     function swap(address account, address input, address output, uint256 amount) external;
-    function splitReward(uint amount) external view returns (uint helixAmount, uint apAmount);
-    function getAmountOut(address tokenIn, uint amountIn, address tokenOut) external view returns (uint amountOut);
+    function splitReward(uint256 amount) external view returns (uint256 helixAmount, uint256 apAmount);
+    function getAmountOut(address tokenIn, uint256 amountIn, address tokenOut) external view returns (uint256 amountOut);
 }
