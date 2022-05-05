4bfe8f663ca26b723931c1474a6e1fe6e14bd227
Topdevbeast <topdevbeast@gmail.com>
2022-05-05 08:25:37 -0700

    update error handling

--- mocha_tests/HelixFactory.test.ts
+++ mocha_tests/HelixFactory.test.ts
@@ -72,20 +72,20 @@ describe('HelixFactory', () => {
   it('factory: createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2985636)
+    expect(receipt.gasUsed).to.eq(2972795)
   })
 
   it('factory: setFeeTo', async () => {
-    await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('Helix: FORBIDDEN')
+    await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('Factory: not feeToSetter')
     await factory.setFeeTo(wallet.address)
     expect(await factory.feeTo()).to.eq(wallet.address)
   })
 
   it('factory: setFeeToSetter', async () => {
-    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('Helix: FORBIDDEN')
+    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('Factory: not feeToSetter')
     await factory.setFeeToSetter(other.address)
     expect(await factory.feeToSetter()).to.eq(other.address)
-    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('Helix: FORBIDDEN')
+    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('Factory: not feeToSetter')
   })
 
   })
