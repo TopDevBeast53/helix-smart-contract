03b501bcdd0a0995799c82f3daa85732c1ced3d4
Topdevbeast <topdevbeast@gmail.com>
2022-05-11 08:15:27 -0700

    initial upgradeable helix pair

--- mocha_tests/HelixFactory.test.ts
+++ mocha_tests/HelixFactory.test.ts
@@ -72,7 +72,7 @@ describe('HelixFactory', () => {
   it('factory: createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2308421)
+    expect(receipt.gasUsed).to.eq(2414693)
   })
 
   it('factory: setFeeTo', async () => {
