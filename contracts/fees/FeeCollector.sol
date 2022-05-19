3744b9eab8db4e23d2dd1563088ee59c05723bdd
Topdevbeast <topdevbeast@gmail.com>
2022-05-19 13:07:51 -0700

    split feeCollector into feeCollector and feeHandler

--- contracts/fees/FeeCollector.sol
+++ contracts/fees/FeeCollector.sol
@@ -1,81 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "../libraries/Percent.sol";
+import "../interfaces/IFeeHandler.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract FeeCollector is Ownable {
-    /// Owner defined fee recipient
-    address public treasury;
+abstract contract FeeCollector is Ownable {
+    /// Handler that this collector transfers fees to
+    IFeeHandler public feeHandler;
 
-    /// Owner defined pool where fees can be staked
-    address public nftChef;
-
-    /// Determines the total percent taken by the collector
+    /// Determines the fee percent taken by this collector
     uint256 public collectorPercent;
 
-    /// Determines percentage of collector fees sent to nftChef
-    uint256 public nftChefPercent;
-
-    // Emitted when a new treasury address is set by the owner
-    event SetTreasury(address _treasury);
-
-    // Emitted when a new nftChef address is set by the owner
-    event SetNftChef(address _nftChef);
+    // Emitted when a new _feeHandler address is set by the owner
+    event SetFeeHandler(address _feeHandler);
 
     // Emitted when a new collector percent is set by the owner
     event SetCollectorPercent(uint256 _collectorPercent);
 
-    // Emitted when a new nftChef percent is set by the owner
-    event SetNftChefPercent(uint256 _nftChefPercent);
+    /// Delegate feeHandler to transfer _fee amount of _token from _from
+    function delegateTransfer(IERC20 _token, address _from, uint256 _fee) internal {
+        require(_fee > 0, "FeeCollector: zero fee");
+        require(address(feeHandler) != address(0), "FeeCollector: handler not set");
+        _token.approve(address(feeHandler), _fee);
+        feeHandler.transferFee(_token, _from, _fee);
+    }
 
-    /// Return true if the treasury address is set and false otherwise
-    function isTreasurySet() external view returns (bool) {
-        return treasury != address(0);
+    /// Return true if the feeHandler address is set and false otherwise
+    function isFeeHandlerSet() external view returns (bool) {
+        return feeHandler != address(0);
     }
     
-    /// Return true if the nftChef address is set and false otherwise
-    function isNftChefSet() external view returns (bool) {
-        return nftChef != address(0);
-    }
-
-    /// Called by the owner to set a new _treasury address
-    function setTreasury(address _treasury) external onlyOwner { 
-        _requireValidAddress(_treasury);
-        treasury = _treasury;
-        emit SetTreasury(_treasury);
-    }
-
-    /// Called by the owner to set a new _nftChef address
-    function setNftChef(address _nftChef) external onlyOwner {
-        _requireValidAddress(_nftChef);
-        nftChef = _nftChef;
-        emit SetNftChef(_nftChef);
+    /// Called by the owner to set a new _feeHandler address
+    function setFeeHandler(IFeeHandler _feeHandler) external onlyOwner { 
+        feeHandler = _feeHandler;
+        emit SetFeeHandler(address(_feeHandler));
     }
 
     /// Called by the owner to set the _collectorPercent collected from transactions
     function setCollectorPercent(uint256 _collectorPercent) external onlyOwner {
-        _requireValidPercent(_collectorPercent);
+        require(Percent.isValidPercent(_collectorPercent), "FeeCollector: percent exceeds max");
         collectorPercent = _collectorPercent;
         emit SetCollectorPercent(_collectorPercent);
     }
 
-    /// Called by the owner to set the _nftChefPercent taken from the total collectorFee
-    /// and staked with the nftChef
-    function setNftChefPercent(uint256 _nftChefPercent) external onlyOwner {
-        _requireValidPercent(_nftChefPercent);
-        nftChefPercent = _nftChefPercent;
-        emit SetNftChefPercent(_nftChefPercent);
-    }
-
     /// Return the collector fee computed from the _amount and the collectorPercent
     function getCollectorFee(uint256 _amount) public view returns (uint256 collectorFee) {
-        collectorFee = _getFee(_amount, collectorPercent);
-    }
-
-    /// Return the nftChef fee computed from the _amount and the nftChefPercent
-    function getNftChefFee(uint256 _amount) public view returns (uint256 nftChefFee) {
-        nftChefFee = _getFee(_amount, nftChefPercent);
+        collectorFee = Percent.getPercentage(_amount, collectorPercent);
     }
 
     /// Split _amount based on collectorPercent and return the collectorFee and the remainder
@@ -85,45 +58,6 @@ contract FeeCollector is Ownable {
         view 
         returns (uint256 collectorFee, uint256 remainder) 
     {
-        (collectorFee, remainder) = _getSplit(_amount, collectorPercent);
-    }
-
-    /// Split _amount based on nftChefPercent and return the nftChefFee and the remainder
-    /// where remainder == _amount - nftChefFee
-    function getNftChefFeeSplit(uint256 _amount)
-        public
-        view
-        returns (uint256 nftChefFee, uint256 remainder)
-    {
-        (nftChefFee, remainder) = _getSplit(_amount, nftChefPercent);
-    }
-
-    /// Return the fee computed from the _amount and the _percent
-    function _getFee(uint256 _amount, uint256 _percent) 
-        public 
-        view 
-        returns (uint256 fee) 
-    {
-        fee = Percent.getPercentage(_amount, _percent);
-    }
-
-    /// Split _amount based on _percent and return the fee and the remainder
-    /// where remainder == _amount - fee
-    function _getSplit(uint256 _amount, uint256 _percent) 
-        private 
-        pure 
-        returns (uint256 fee, uint256 remainder)
-    {
-        (fee, remainder) = Percent.splitByPercent(_amount, _percent);
-    }
-
-    // Require that the _address is valid
-    function _requireValidAddress(address _address) private pure {
-        require(_address != address(0), "FeeCollector: zero address");
-    }
-
-    // Require that the _percent is valid
-    function _requireValidPercent(uint256 _percent) private pure {
-        require(Percent.isValidPercent(_percent), "FeeCollector: percent exceeds max");
+        (collectorFee, remainder) = Percent.splitByPercent(_amount, collectorFee);
     }
 }
---
+++ contracts/fees/FeeHandler.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "../libraries/Percent.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// Handles routing received fees to internal contracts
+contract FeeHandler is Initializable, OwnableUpgradeable {
+    /// Owner defined fee recipient
+    address public treasury;
+
+    /// Owner defined pool where fees can be staked
+    address public nftChef;
+
+    /// Determines percentage of collector fees sent to nftChef
+    uint256 public nftChefPercent;
+
+    // Emitted when a new treasury address is set by the owner
+    event SetTreasury(address _treasury);
+
+    // Emitted when a new nftChef address is set by the owner
+    event SetNftChef(address _nftChef);
+
+    // Emitted when a new nftChef percent is set by the owner
+    event SetNftChefPercent(uint256 _nftChefPercent);
+
+    modifier onlyValidFee(uint256 _fee) {
+        require(_fee > 0, "FeeHandler: zero fee");
+        _;
+    }
+
+    modifier onlyValidAddress(address _address) {
+        require(_address != address(0), "FeeCollector: zero address");
+        _;
+    }
+
+    modifier onlyValidPercent(uint256 _percent) {
+        require(Percent.isValidPercent(_percent), "FeeCollector: percent exceeds max");
+        _;
+    } 
+
+    function initialize(address _treasury, address _nftChef) external initializer {
+        __Ownable_init();
+        treasury = _treasury;
+        nftChef = _nftChef;
+    }
+    
+    /// Called by a FeeCollector to send _amount of _token to this FeeHandler
+    /// handles sending fees to treasury and staking with nftChef
+    function transferFee(IERC20 _token, uint256 _fee) external onlyValidFee(_fee) {
+        (uint256 nftChefAmount, uint256 treasuryAmount) = _getSplit(_fee, nftChefPercent);
+
+        if (nftChefAmount > 0) {
+            _token.transferFrom(msg.sender, address(nftChef), nftChefAmount);
+            // TODO call the nft yield update compute function
+        }
+
+        if (treasuryAmount > 0) {
+            _token.transferFrom(msg.sender, address(treasury), treasuryAmount);
+        }
+    }
+
+    /// Called by the owner to set a new _treasury address
+    function setTreasury(address _treasury) external onlyOwner onlyValidAddress(_treasury) { 
+        treasury = _treasury;
+        emit SetTreasury(_treasury);
+    }
+
+    /// Called by the owner to set a new _nftChef address
+    function setNftChef(address _nftChef) 
+        external 
+        onlyOwner 
+        onlyValidAddress(_nftChef) 
+    {
+        nftChef = _nftChef;
+        emit SetNftChef(_nftChef);
+    }
+
+    /// Called by the owner to set the _nftChefPercent taken from the total collector fees
+    /// and staked with the nftChef
+    function setNftChefPercent(uint256 _nftChefPercent) 
+        external 
+        onlyOwner 
+        onlyValidPercent(_nftChefPercent) 
+    {
+        nftChefPercent = _nftChefPercent;
+        emit SetNftChefPercent(_nftChefPercent);
+    }
+
+    /// Return the nftChef fee computed from the _amount and the nftChefPercent
+    function getNftChefFee(uint256 _amount) external view returns (uint256 nftChefFee) {
+        nftChefFee = _getFee(_amount, nftChefPercent);
+    }
+
+    /// Split _amount based on nftChefPercent and return the nftChefFee and the remainder
+    /// where remainder == _amount - nftChefFee
+    function getNftChefFeeSplit(uint256 _amount)
+        external 
+        view
+        returns (uint256 nftChefFee, uint256 remainder)
+    {
+        (nftChefFee, remainder) = _getSplit(_amount, nftChefPercent);
+    }
+
+    /// Return the fee computed from the _amount and the _percent
+    function _getFee(uint256 _amount, uint256 _percent) 
+        private 
+        view 
+        returns (uint256 fee) 
+    {
+        fee = Percent.getPercentage(_amount, _percent);
+    }
+
+    /// Split _amount based on _percent and return the fee and the remainder
+    /// where remainder == _amount - fee
+    function _getSplit(uint256 _amount, uint256 _percent) 
+        private 
+        pure 
+        returns (uint256 fee, uint256 remainder)
+    {
+        (fee, remainder) = Percent.splitByPercent(_amount, _percent);
+    }
+}
