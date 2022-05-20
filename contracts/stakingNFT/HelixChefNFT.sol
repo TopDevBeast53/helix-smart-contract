3e7718462a66944596bcbed66174991b52312700
Topdevbeast <topdevbeast@gmail.com>
2022-05-20 16:02:56 -0700

    clean up and conventionalize contract

--- contracts/stakingNFT/HelixChefNFT.sol
+++ contracts/stakingNFT/HelixChefNFT.sol
@@ -5,30 +5,38 @@ import "../interfaces/IHelixNFT.sol";
 import "../tokens/HelixToken.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract HelixChefNFT is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract HelixChefNFT is 
+    Initializable, 
+    OwnableUpgradeable, 
+    ReentrancyGuardUpgradeable,
+    PausableUpgradeable
+{
+    // Info on each user who has NFTs staked in this contract
+    struct UserInfo {
+        uint[] stakedNFTsId;        // Ids of the NFTs this user has staked
+        uint accrued;               // TODO: Accrued but unwithdrawn from percentage of yield
+    }
 
-    // instance of HelixNFT
+    // Instance of HelixNFT
     IHelixNFT private helixNFT;
 
     /// Token deposited into this contract
     HelixToken public token;
 
-    // Info of each user who took part in staking
-    struct UserInfo {
-        // list of staked NFT's ID
-        uint[] stakedNFTsId;
-        uint accrued; // TODO: accrued Helix from percentage of yield
-    }
-
+    /// Total number of NFTs staked in this contract
     uint totalAmountStakedNfts; // TODO:
 
-    // users took part in staking : UserAddress => UserInfo
+    /// Maps a user's address to their info struct
     mapping (address => UserInfo) public users;
 
-    event StakeTokens(address indexed user, uint[] tokensId);
-    event UnstakeToken(address indexed user, uint[] tokensId);
+    // Emitted when an NFTs are staked
+    event StakeTokens(address indexed user, uint256[] tokenIds);
+
+    // Emitted when an NFTs are unstaked
+    event UnstakeTokens(address indexed user, uint256[] tokenIds);
 
     function initialize(IHelixNFT _helixNFT, HelixToken _token) external initializer {
         __Ownable_init();
@@ -37,105 +45,92 @@ contract HelixChefNFT is Initializable, OwnableUpgradeable, ReentrancyGuardUpgra
         token = _token;
     }
 
-    //Public functions -------------------------------------------------------
-
-    /**
-     * @dev staking
-     */
-    function stake(uint256[] memory tokensId) public nonReentrant {
-        _withdrawRewardToken();// --------1
+    /// Stake the tokens with _tokenIds in the pool
+    function stake(uint256[] memory _tokenIds) external whenNotPaused nonReentrant {
+        _withdrawRewardToken();
         
         UserInfo storage user = users[msg.sender];
 
-        for(uint256 i = 0; i < tokensId.length; i++){
-            (address tokenOwner, bool isStaked) = helixNFT.getInfoForStaking(tokensId[i]);
+        for(uint256 i = 0; i < _tokenIds.length; i++){
+            (address tokenOwner, bool isStaked) = helixNFT.getInfoForStaking(_tokenIds[i]);
             _requireIsTokenOwner(msg.sender, tokenOwner);
             require(!isStaked, "HelixChefNFT: already staked");
-            helixNFT.setIsStaked(tokensId[i], true);
-            user.stakedNFTsId.push(tokensId[i]);// --------2
+
+            helixNFT.setIsStaked(_tokenIds[i], true);
+            user.stakedNFTsId.push(_tokenIds[i]);
         }
-        emit StakeTokens(msg.sender, tokensId);
+
+        emit StakeTokens(msg.sender, _tokenIds);
     }
 
-    /**
-     * @dev unstaking
-     */
-    function unstake(uint256[] memory tokensId) public nonReentrant {
+    /// Unstake the tokens with _tokenIds in the pool
+    function unstake(uint256[] memory _tokenIds) external whenNotPaused nonReentrant {
+        _withdrawRewardToken();
         
-        _withdrawRewardToken();// --------1
-        
-        for(uint256 i = 0; i < tokensId.length; i++){
-            (address tokenOwner, bool isStaked) = helixNFT.getInfoForStaking(tokensId[i]);
+        for(uint256 i = 0; i < _tokenIds.length; i++){
+            (address tokenOwner, bool isStaked) = helixNFT.getInfoForStaking(_tokenIds[i]);
             _requireIsTokenOwner(msg.sender, tokenOwner);
             require(isStaked, "HelixChefNFT: already unstaked");
-            helixNFT.setIsStaked(tokensId[i], false);
-            removeTokenIdFromUsers(tokensId[i], msg.sender);// --------2
+
+            helixNFT.setIsStaked(_tokenIds[i], false);
+            _removeTokenIdFromUsers(_tokenIds[i], msg.sender);
         }
         
-        emit UnstakeToken(msg.sender, tokensId);
+        emit UnstakeTokens(msg.sender, _tokenIds);
     }
 
-    //External functions -----------------------------------------------------
-
-    /**
-     * @dev To get the amount staked by user
-     */
-    function getAmountStaked(address user) external view returns(uint) {
-        return users[user].stakedNFTsId.length;
+    /// Return the number of NFTs the _user has staked
+    function getAmountStaked(address _user) external view returns(uint) {
+        return users[_user].stakedNFTsId.length;
     }
 
-    /**
-     * @dev To withdraw reward token
-     */
+    /// Withdraw accrued reward token
     function withdrawRewardToken() external nonReentrant {
         _withdrawRewardToken();
     }
-
-    /**
-     * @dev See the list of the NFT ids that `_user` has staked
-     */
-    function getUserStakedTokens(address _user) external view returns(uint[] memory){
-        uint[] memory tokensId = new uint[](users[_user].stakedNFTsId.length);
-        tokensId = users[_user].stakedNFTsId;
-        return tokensId;
+    
+    /// Called by the owner to pause the contract
+    function pause() external onlyOwner {
+        _pause();
     }
 
-    /**
-     * @dev See pending Reward on frontend.
-     */
+    /// Called by the owner to unpause the contract
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// Return the array of NFT ids that _user has staked
+    function getUserStakedTokens(address _user) external view returns(uint[] memory){
+        uint[] memory tokenIds = new uint[](users[_user].stakedNFTsId.length);
+        tokenIds = users[_user].stakedNFTsId;
+        return tokenIds;
+    }
+
+    /// Return the _user's pending reward
     function pendingReward(address _user) external view returns (uint) {
         return users[_user].accrued;
     }
-
-    //internal functions -----------------------------------------------------
-
-    /**
-     * @dev Withdraw rewardToken from HelixChefNFT.
-     *
-     */
+    // Withdraw accrued reward token
     function _withdrawRewardToken() internal {
         uint _amount = users[msg.sender].accrued;
         users[msg.sender].accrued = 0;
         token.transfer(address(msg.sender), _amount);
     }
 
-    /**
-     * @dev Remove TokenId to be unstaked from userInfo
-     * @param tokenId to be removed
-     * @param user who is unstaking
-     */
-    function removeTokenIdFromUsers(uint256 tokenId, address user) internal {
-        uint[] storage tokensId = users[user].stakedNFTsId;
-        for (uint256 i = 0; i < tokensId.length; i++) {
-            if (tokenId == tokensId[i]) {
-                tokensId[i] = tokensId[tokensId.length - 1];
-                tokensId.pop();
+    // Remove _tokenId from _user's account
+    function _removeTokenIdFromUsers(uint256 _tokenId, address _user) private {
+        uint[] storage tokenIds = users[_user].stakedNFTsId;
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            if (_tokenId == tokenIds[i]) {
+                tokenIds[i] = tokenIds[tokenIds.length - 1];
+                tokenIds.pop();
                 return;
             }
         }
     }
 
-    function _requireIsTokenOwner(address caller, address tokenOwner) private pure {
-            require(caller == tokenOwner, "HelixChefNFT: not token owner");
+    // Require that _caller is _tokenOwner
+    function _requireIsTokenOwner(address _caller, address _tokenOwner) private pure {
+            require(_caller == _tokenOwner, "HelixChefNFT: not token owner");
     }
 }
