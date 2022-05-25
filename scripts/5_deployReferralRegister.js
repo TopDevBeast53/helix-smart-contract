cd60d40d9a9947bd8b1bb4535823dae3a383c383
Topdevbeast <topdevbeast@gmail.com>
2022-05-24 19:52:54 -0700

    output referral register implementation address

--- scripts/5_deployReferralRegister.js
+++ scripts/5_deployReferralRegister.js
@@ -8,38 +8,46 @@
  *      npx hardhat run scripts/5_deployReferralRegister.js --network rinkeby
  * 
  */
-const { ethers, upgrades } = require(`hardhat`);
+const { ethers, upgrades } = require(`hardhat`)
 const contracts = require("./constants/contracts")
 const initials = require("./constants/initials")
 const env = require("./constants/env")
 
-const HelixTokenAddress = contracts.helixToken[env.network];
-const StakingFeePercent = initials.REFERRAL_STAKING_FEE_PERCENT[env.network];
-const SwapFeePercent = initials.REFERRAL_SWAP_FEE_PERCENT[env.network];
+const HelixTokenAddress = contracts.helixToken[env.network]
+const StakingFeePercent = initials.REFERRAL_STAKING_FEE_PERCENT[env.network]
+const SwapFeePercent = initials.REFERRAL_SWAP_FEE_PERCENT[env.network]
 
 async function main() {
 
-    const [deployer] = await ethers.getSigners();
-    console.log(`Deployer address: ${ deployer.address}`);
+    const [deployer] = await ethers.getSigners()
+    console.log(`Deployer address: ${ deployer.address}`)
 
-    console.log(`------ Start deploying Referral Register contract ---------`);
-    const ReferralRegister = await ethers.getContractFactory(`ReferralRegister`);
-    ref = await upgrades.deployProxy(ReferralRegister, [HelixTokenAddress, StakingFeePercent, SwapFeePercent]);
-    await ref.deployTransaction.wait();
-    console.log(`Referral Register deployed to ${ref.address}`);
+    console.log(`------ Start deploying Referral Register contract ---------`)
+    const ReferralRegister = await ethers.getContractFactory(`ReferralRegister`)
+    ref = await upgrades.deployProxy(
+        ReferralRegister, 
+        [HelixTokenAddress, StakingFeePercent, SwapFeePercent]
+    )
+    await ref.deployTransaction.wait()
+    console.log(`Referral Register deployed to ${ref.address}`)
 
-    console.log(`------ Add Referral Register as Minter to HelixToken ---------`);
-    const HelixToken = await ethers.getContractFactory(`HelixToken`);
-    const helixToken = HelixToken.attach(HelixTokenAddress);
+    const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+        ref.address
+    )
+    console.log(`Implementation address: ${implementationAddress}`)
 
-    let tx = await helixToken.addMinter(ref.address);
-    await tx.wait();
+    console.log(`------ Add Referral Register as Minter to HelixToken ---------`)
+    const HelixToken = await ethers.getContractFactory(`HelixToken`)
+    const helixToken = HelixToken.attach(HelixTokenAddress)
+
+    let tx = await helixToken.addMinter(ref.address)
+    await tx.wait()
     console.log(`Success to add Minter to HelixToken`)
 }
 
  main()
      .then(() => process.exit(0))
      .catch((error) => {
-         console.error(error);
-         process.exit(1);
-     }); 
+         console.error(error)
+         process.exit(1)
+     }) 
