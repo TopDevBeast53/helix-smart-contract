eab8569318a2d4940d9b3b318e9c8dcc1d49c0b4
Topdevbeast <topdevbeast@gmail.com>
2022-05-26 08:32:03 +0800

    fix the nft deployment script and deploy nft, chefnft, bridge

--- scripts/8_deployHelixNFTStaking.js
+++ scripts/8_deployHelixNFTStaking.js
@@ -12,18 +12,20 @@ const { ethers, network, upgrades } = require(`hardhat`)
 const contracts = require("./constants/contracts")
 const env = require("./constants/env")
 
+const rewardToken = contracts.helixToken[env.network];
+
 async function main() {
     const [deployer] = await ethers.getSigners()
     console.log(`Deployer address: ${ deployer.address}`)
     
-    const nonce = await network.provider.send(
+    let nonce = await network.provider.send(
         `eth_getTransactionCount`, 
         [deployer.address, "latest"]
     )
     
     console.log(`Deploy Upgradeable Helix NFT`)
     const helixNftFactory = await ethers.getContractFactory(`HelixNFT`)
-    helixNftProxy = await upgrades.deployProxy(HelixNftFactory, [``], {nonce: nonce})
+    const helixNftProxy = await upgrades.deployProxy(helixNftFactory, [``], {nonce: nonce++})
     await helixNftProxy.deployTransaction.wait()
     console.log(`Helix NFT proxy address: ${helixNftProxy.address}`)
 
@@ -36,8 +38,8 @@ async function main() {
     const HelixChefNftFactory = await ethers.getContractFactory(`HelixChefNFT`)
     const helixChefNftProxy = await upgrades.deployProxy(
         HelixChefNftFactory, 
-        [helixNft.address, rewardToken], 
-        {nonce: nonce}
+        [helixNftProxy.address, rewardToken], 
+        {nonce: nonce++}
     )
     await helixChefNftProxy.deployTransaction.wait()
     console.log(`Helix Chef NFT Proxy address: ${helixChefNftProxy.address}`)
@@ -48,14 +50,12 @@ async function main() {
     console.log(`Helix Chef NFT Implementation address: ${helixChefNftImplementationAddress}`)   
 
     console.log(`Add helix chef nft as staker to helix nft`)
-    let tx = await helixNft.addStaker(helixChefNft.address, {gasLimit: 3000000})
+    let tx = await helixNftProxy.addStaker(helixChefNftProxy.address, {gasLimit: 3000000})
     await tx.wait()
     
-    console.log(`Add deployer address as minter to helix nft`)
-    tx = await helixNft.addMinter(deployer.address, {gasLimit: 3000000})
-    await tx.wait()
-
-    console.log(`Do NOT forget to fund the newly deployed HelixNFTChef with reward token!`)
+    // console.log(`Add deployer address as minter to helix nft`)
+    // tx = await helixNftProxy.addMinter(deployer.address, {gasLimit: 3000000, nonce: nonce++})
+    // await tx.wait()
 }
 
 main()
