be56cf153eb5cecf383082b714e86d6c897ef2f6
Topdevbeast <topdevbeast@gmail.com>
2022-06-03 08:56:34 -0700

    add external contracts for testing migrate liquidity

---
+++ scripts/constants/externalContracts.js
@@ -0,0 +1,12 @@
+// Contracts used for testing migrate liquidity
+module.exports = {
+    factory: {
+        4: '0x4e49d75C6b0EC8528deCfF5fE7834efB9d000063',
+    },
+    factoryImplementation: {
+        4: '0xB8B179DC12915316b94eAB4fa4b219D6762D04Cd',
+    },
+    router: {
+        4: '0xb8B2334E8A6E355746f397Cc004A4D2e52887A6B',
+    },
+}
