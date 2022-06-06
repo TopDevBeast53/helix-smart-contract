c61da768f8630174c0ee32d110a19e35545e2af5
Topdevbeast <topdevbeast@gmail.com>
2022-06-06 11:13:38 +0800

    test script nft chef

--- scripts/testNftChef.js
+++ scripts/testNftChef.js
@@ -12,6 +12,7 @@
  
  
  const helixChefNFTAddress = contracts.helixChefNFT[env.network];
+ const helixNFTAddress = contracts.helixNFT[env.network];
  const helixToken = contracts.helixToken[env.network];
  
  async function main() {
@@ -21,13 +22,19 @@
      const IHelixToken = await ethers.getContractFactory("HelixToken");
      const helix = IHelixToken.attach(helixToken).connect(admin);
  
+     const IHelixNFT = await ethers.getContractFactory("HelixNFT");
+     const helixNFT = IHelixNFT.attach(helixNFTAddress).connect(admin);
+ 
      const IHelixChefNFT = await ethers.getContractFactory("HelixChefNFT");
      const chefNFT = IHelixChefNFT.attach(helixChefNFTAddress).connect(admin);
  
-     console.log("TotalStaked:", (await chefNFT.totalStakedNfts()).toString());
-    //  console.log("User staked:", (await chefNFT.users('0x59201fb8cb2D61118B280c8542127331DD141654')));
-    console.log("usersStakedNfts:", (await chefNFT.usersStakedNfts('0x59201fb8cb2D61118B280c8542127331DD141654')).toString());
-     console.log("getAccruedReward:", (await chefNFT.getAccruedReward('0x59201fb8cb2D61118B280c8542127331DD141654', 1000)))
+    //  console.log("TotalStaked:", (await chefNFT.totalStakedNfts()).toString());
+    //  console.log("User staked:", (await chefNFT.users('0xCC8b0d940F3C450593b06e92e74C96b7908765f1')));
+    // console.log("usersStakedNfts:", (await chefNFT.usersStakedNfts('0xCC8b0d940F3C450593b06e92e74C96b7908765f1')).toString());
+    console.log("token ids:", (await helixNFT.getTokenIdsOfOwner('0xCC8b0d940F3C450593b06e92e74C96b7908765f1')))
+    console.log("token info of id: 9", (await helixNFT.getToken('9')))
+    console.log("token info of id: 12", (await helixNFT.getToken('12')))
+    
  }
  
  main()
