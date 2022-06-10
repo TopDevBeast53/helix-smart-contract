05236d2027180cc50be2dccf8ed3a4e8f51ee07a
Topdevbeast <topdevbeast@gmail.com>
2022-06-10 13:43:35 -0700

    remove create pair with swap fee arg

--- contracts/swaps/HelixFactory.sol
+++ contracts/swaps/HelixFactory.sol
@@ -65,25 +65,18 @@ contract HelixFactory is Initializable {
         return allPairs.length;
     }
 
-    function createPair(address tokenA, address tokenB) external returns (address) {
-        return createPair(tokenA, tokenB, defaultSwapFee);
-    }
-
-    function createPair(address tokenA, address tokenB, uint32 swapFee) 
-        public 
-        returns (address pair) 
-    {
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
         if (tokenA == tokenB) revert IdenticalTokens();
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         if (token0 == address(0)) revert ZeroAddress();
         if (getPair[token0][token1] != address(0)) revert PairAlreadyExists(token0, token1);
 
         bytes memory bytecode = type(HelixPair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1, swapFee));
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        HelixPair(pair).initialize(token0, token1, swapFee);
+        HelixPair(pair).initialize(token0, token1, defaultSwapFee);
 
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
