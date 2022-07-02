ab978f2c490816554b54aa5c26982505501cc2cd
Topdevbeast <topdevbeast@gmail.com>
2022-07-03 06:49:01 +0800

    add import

--- contracts/tokens/HelixNFT.sol
+++ contracts/tokens/HelixNFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
