65aabe81f1534a66d1704fab4da674abbc6c9521
Topdevbeast <topdevbeast@gmail.com>
2022-07-25 10:45:50 -0700

    update to deploy to rsk testnet

--- constants/env.js
+++ constants/env.js
@@ -1,20 +1,20 @@
 module.exports = {
-    // bscMainNet:56
-    // bscTestNet:97
-    // EthereumMainNet:1
-    // Rinkeby:4
-    // Ropsten:3
-    // Goerli:5
-    network: 1,
+    // network: 1      // Ethereum Mainnet
+    // network: 3      // Ropsten Testnet
+    // network: 4      // Rinkeby Testnet
+    // network: 5      // Goerli Testnet
+    network: 31     // RSK Testnet
+    // network: 56     // BSC Mainnet
+    // network: 97     // BSC Testnet
 
-    // bscMainNet: https://bsc-dataseed1.binance.org
-    // bscTestNet: https://data-seed-prebsc-1-s1.binance.org:8545
-    // EthereumMainNet: https://eth-mainnet.g.alchemy.com/v2/VqUJXhZEYxUqofUHG6UWgxbuD3tw3yCf
-    // Rinkeby: https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF
-    // Ropsten: https://ropsten.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658
-    // Goerli: https://goerli.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658
-    // rpcURL: 'https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF',
-    rpcURL: 'https://eth-mainnet.g.alchemy.com/v2/VqUJXhZEYxUqofUHG6UWgxbuD3tw3yCf',
+
+    // rpcURL: https://eth-mainnet.g.alchemy.com/v2/VqUJXhZEYxUqofUHG6UWgxbuD3tw3yCf   // Ethererum Mainnet
+    // rpcURL: https://ropsten.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658           // Ropsten Testnet
+    // rpcURL: https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF   // Rinkeby Testnet
+    // rpcURL: https://goerli.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658            // Goerli Testnet
+    rpcURL: https://public-node.testnet.rsk.co/                                     // RSK Testnet
+    // rpcURL: https://bsc-dataseed1.binance.org                                       // BSC Mainnet
+    // rpcURL: https://data-seed-prebsc-1-s1.binance.org:8545                          // BSC Testnet
 
     testNetwork: 4,
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -16,11 +16,11 @@ const alchemyURL = process.env.ALCHEMY_URL;
 const privateKey = process.env["PRIVATE_KEY"];
 
 task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+    const accounts = await ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 function getAccounts() {
@@ -37,86 +37,89 @@ function getAccounts() {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        },
+        hardhat: {
+            blockGasLimit: 99999999
+        },
+        frame: {
+            url: 'http://localhost:1248'
+        },
+        testnetBSC: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+            chainId: 97,
+            gasPrice: 20000000000,
+            gas: 2100000,
+            accounts: getAccounts(),
+        },
+        mainnetBSC: {
+            url: "https://bsc-dataseed1.binance.org",
+            chainId: 56,
+            gasPrice: 20000000000,
+            gas: 2100000,
+            accounts: getAccounts(),
+        },
+        rinkeby: {
+            url: rinkebyURL || "",
+            chainId: 4,
+            gasPrice: 5000000000,
+            accounts: getAccounts(),
+        },
+        goerli: {
+            url: goerliURL || "",
+            chainId: 5,
+            gasPrice: 5000000000,
+            accounts: getAccounts(),
+        },
+        ropsten: {
+            url: ropstenURL || "",
+            chainId: 3,
+            gasPrice: 5000000000,
+            accounts: getAccounts(),
+        },
+        mainnetETH: {
+            url: alchemyURL || "",
+            chainId: 1,
+            gasPrice: 15000000000,
+            accounts: getAccounts(),
+        },
+        rskTestnet: {
+            chainId: 31,
+            url: 'https://public-node.testnet.rsk.co/',
+            gasPrice: 5000000000,
+            accounts: getAccounts(),
+        },
     },
-    hardhat: {
-      blockGasLimit: 99999999
+    solidity: {
+        compilers:[
+            {
+                version: "0.8.10",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    }
+                }
+            }
+        ]
     },
-    frame: {
-      url: 'http://localhost:1248'
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
     },
-    testnetBSC: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      gas: 2100000,
-      accounts: getAccounts(),
+    etherscan: {
+        apiKey: etherscanApiKey
     },
-    mainnetBSC: {
-      url: "https://bsc-dataseed1.binance.org",
-      chainId: 56,
-      gasPrice: 20000000000,
-      gas: 2100000,
-      accounts: getAccounts(),
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
     },
-    rinkeby: {
-      url: rinkebyURL || "",
-      chainId: 4,
-      gasPrice: 5000000000,
-      accounts: getAccounts(),
-    },
-    goerli: {
-      url: goerliURL || "",
-      chainId: 5,
-      gasPrice: 5000000000,
-      accounts: getAccounts(),
-    },
-
-    ropsten: {
-      url: ropstenURL || "",
-      chainId: 3,
-      gasPrice: 5000000000,
-      accounts: getAccounts(),
-    },
-
-    mainnetETH: {
-      url: alchemyURL || "",
-      chainId: 1,
-      gasPrice: 15000000000,
-      accounts: getAccounts(),
+    mocha: {
+        timeout: 200000
     }
-  },
-  solidity: {
-    compilers:[
-      {
-        version: "0.8.10",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          }
-        }
-      }
-
-    ]
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: etherscanApiKey
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 200000
-  }
-};
+}
