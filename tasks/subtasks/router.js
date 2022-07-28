d319576c9c43185862eb1c4396da8bd08f6823c0
Topdevbeast <topdevbeast@gmail.com>
2022-07-28 13:34:08 -0700

    add router subtasks

--- tasks/subtasks/router.js
+++ tasks/subtasks/router.js
@@ -12,84 +12,495 @@ const contract = async () => await hre.run("loadContract", { name: name, address
 // READ
 
 
-// WETH
 subtask("router.WETH")
     .setAction(async () => {
         const result = await (await contract()).WETH()
         console.log(result.toString())
     })
 
-// _WETH
+subtask("router._WETH")
+    .setAction(async () => {
+        const result = await (await contract())._WETH()
+        console.log(result.toString())
+    })
 
-// _factory
+subtask("router._factory")
+    .setAction(async () => {
+        const result = await (await contract())._factory()
+        console.log(result.toString())
+    })
 
-// factory
+subtask("router.factory")
+    .setAction(async () => {
+        const result = await (await contract()).factory()
+        console.log(result.toString())
+    })
 
-// getAmountIn
+subtask("router.getAmountIn")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountIn(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3
+        )
+        console.log(result.toString())
+    })
 
-// getAmountIn
+subtask("router.getAmountIn")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountIn(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+        )
+        console.log(result.toString())
+    })
 
-// getAmountOut
+subtask("router.getAmountOut")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountOut(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+        )
+        console.log(result.toString())
+    })
 
-// getAmountOut
+subtask("router.getAmountOut")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountOut(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3
+        )
+        console.log(result.toString())
+    })
 
-// getAmountsIn
+subtask("router.getAmountsIn")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountsIn(
+            args.arg0,
+            args.arg1
+        )
+        console.log(result.toString())
+    })
 
-// getAmountsOut
+subtask("router.getAmountsOut")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).getAmountsOut(
+            args.arg0,
+            args.arg1
+        )
+        console.log(result.toString())
+    })
 
-// owner
+subtask("router.owner")
+    .setAction(async () => {
+        const result = await (await contract()).owner()
+        console.log(result.toString())
+    })
 
-// paused
+subtask("router.paused")
+    .setAction(async () => {
+        const result = await (await contract()).paused(
+        console.log(result.toString())
+    })
 
-// quote
+subtask("router.quote")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .setAction(async (args) => {
+        const result = await (await contract()).quote(
+            args.arg0,
+            args.arg1,
+            args.arg2
+        )
+        console.log(result.toString())
+    })
 
-// swapRewards
+subtask("router.swapRewards")
+    .setAction(async () => {
+        const result = await (await contract()).swapRewards(
+        console.log(result.toString())
+    })
+
 
 
 // WRITE
 
 
+subtask("router.addLiquidity")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .addPositionalParam("arg7")
+    .setAction(async (args) => {
+        const result = await (await contract()).addLiquidity(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6,
+            args.arg7
+        )
+    })
 
-// addLiquidity
+subtask("router.addLiquidityETH")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .setAction(async (args) => {
+        const result = await (await contract()).addLiquidityETH(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6
+        )
+    })
 
-// addLiquidityETH
+subtask("router.pause")
+    .setAction(async () => {
+        const result = await (await contract()).pause()
+    })
 
-// pause
+subtask("router.removeLiquidity")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidity(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6
+        )
+    })
 
-// removeLiquidity
+subtask("router.removeLiquidityETH")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidityETH(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5
+        )
+    })
 
-// removeLiquidityETH
+subtask("router.removeLiquidityETHSupportingFeeOnTranferTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidityETHSupportingFeeOnTranferTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5
+        )
+    })
 
-// removeLiquidityETHSupportingFeeOnTranferTokens
+subtask("router.removeLiquidityETHWithPermit")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .addPositionalParam("arg7")
+    .addPositionalParam("arg8")
+    .addPositionalParam("arg9")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidityETHWithPermit(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6,
+            args.arg7,
+            args.arg8,
+            args.arg9,
+        )
+    })
 
-// removeLiquidityETHWithPermit
+subtask("router.removeLiquidityETHWithPermitSupportingFeeOnTransferTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .addPositionalParam("arg7")
+    .addPositionalParam("arg8")
+    .addPositionalParam("arg9")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6,
+            args.arg7,
+            args.arg8,
+            args.arg9,
+        )
+    })
 
-// removeLiquidityETHWithPermitSupportingFeeOnTransferTokens
+subtask("router.removeLiquidityWithPermit")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .addPositionalParam("arg5")
+    .addPositionalParam("arg6")
+    .addPositionalParam("arg7")
+    .addPositionalParam("arg8")
+    .addPositionalParam("arg9")
+    .addPositionalParam("arg10")
+    .setAction(async (args) => {
+        const result = await (await contract()).removeLiquidityWithPermit(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+            args.arg5,
+            args.arg6,
+            args.arg7,
+            args.arg8,
+            args.arg9,
+            args.arg10,
+        )
+    })
 
-// removeLiquidityWithPermit
+subtask("router.renounceOwnership")
+    .setAction(async () => {
+        const result = await (await contract()).renounceOwnership()
+    })
 
-// renounceOwnership
+subtask("router.setSwapReward")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setSwapRewards(
+            args.arg0,
+        )
+    })
 
-// setSwapRewards
+subtask("router.swapETHForExactTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapETHForExactTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapETHForExactTokens
+subtask("router.swapExactETHForTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactETHForTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactETHForTokens
+subtask("router.swapExactETHForTokensSupportingFeeOnTransferTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactETHForTokensSupportingFeeOnTransferTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactETHForTokensSupportingFeeOnTransferTokens
+subtask("router.swapExactTokensForETH")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactTokensForETH(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactTokensForETH
+subtask("router.swapExactTokensForETHSupportingFeeOnTransferTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactTokensForETHSupportingFeeOnTransferTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactTokensForETHSupportingFeeOnTransferTokens
+subtask("router.swapExactTokensForTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactTokensForTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactTokensForTokens
+subtask("router.swapExactTokensForTokensSupportingFeeOnTransferTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapExactTokensForTokensSupportingFeeOnTransferTokens
+subtask("router.swapTokensForExactETH")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapTokensForExactETH(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapTokensForExactETH
+subtask("router.swapTokensForExactTokens")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .addPositionalParam("arg2")
+    .addPositionalParam("arg3")
+    .addPositionalParam("arg4")
+    .setAction(async (args) => {
+        const result = await (await contract()).swapTokensForExactTokens(
+            args.arg0,
+            args.arg1,
+            args.arg2,
+            args.arg3,
+            args.arg4,
+        )
+    })
 
-// swapTokensForExactTokens
+subtask("router.transferOwnership")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).transferOwnership(
+            args.arg0,
+        )
+    })
 
-// transferOwnership
+subtask("router.")
+    .setAction(async () => {
+        const result = await (await contract()).unpause()
+    })
 
-// unpause
+
