8aefed4d3c6f65d3cae4c5b01a2d32981b6a0fdb
Topdevbeast <topdevbeast@gmail.com>
2022-07-28 12:42:40 -0700

    add oracle factory subtasks

--- tasks/subtasks/oracleFactory.js
+++ tasks/subtasks/oracleFactory.js
@@ -12,48 +12,127 @@ const contract = async () => await hre.run("loadContract", { name: name, address
 // READ
 
 
-// canUpdate
 subtask("oracleFactory.canUpdate")
     .setAction(async () => {
         const result = await (await contract()).canUpdate()
         console.log(result.toString())
     })
 
-// consult
 subtask("oracleFactory.consult")
     .addPositionalParam("arg0")
     .addPositionalParam("arg1")
     .addPositionalParam("arg2")
     .setAction(async (args) => {
-        const result = await (await contract()).consult(args.arg0, args.arg1, args.arg2)
+        const result = await (await contract()).consult(
+            args.arg0, 
+            args.arg1, 
+            args.arg2
+        )
         console.log(result.toString())
     })
 
-// factory
+subtask("oracleFactory.factory")
+    .setAction(async () => {
+        const result = await (await contract()).factory()
+        console.log(result.toString())
+    })
 
-// getOracle
+subtask("oracleFactory.getOracle")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).getOracle(
+            args.arg0, 
+            args.arg1
+        )
+        console.log(result.toString())
+    })
 
-// oracleExists
+subtask("oracleFactory.oracleExists")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).oracleExists(
+            args.arg0, 
+            args.arg1
+        )
+        console.log(result.toString())
+    })
 
-// oracles
+subtask("oracleFactory.oracles")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).oracles(
+            args.arg0, 
+            args.arg1
+        )
+        console.log(result.toString())
+    })
 
-// owner
+subtask("oracleFactory.owner")
+    .setAction(async () => {
+        const result = await (await contract()).owner()
+        console.log(result.toString())
+    })
 
-// period
+subtask("oracleFactory.period")
+    .setAction(async () => {
+        const result = await (await contract()).period()
+        console.log(result.toString())
+    })
+
 
 
 // WRITE
 
 
-// create
+subtask("oracleFactory.create")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).create(
+            args.arg0, 
+            args.arg1
+        )
+    })
 
-// initialize
+subtask("oracleFactory.initialize")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).initialize(
+            args.arg0
+        )
+    })
 
-// renounceOwnership
+subtask("oracleFactory.renounceOwnership")
+    .setAction(async () => {
+        const result = await (await contract()).renounceOwnership()
+    })
 
-// setPeriod
+subtask("oracleFactory.setPeriod")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setPeriod(
+            args.arg0
+        )
+    })
 
-// transferOwnership
+subtask("oracleFactory.transferOwnership")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).transferOwnership(
+            args.arg0
+        )
+    })
 
-// update
+subtask("oracleFactory.update")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).update(
+            args.arg0,
+            args.arg1
+        )
+    })
 
