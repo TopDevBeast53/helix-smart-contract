ec46d37f8f885190676b04c1d5bf305ff8a9d77e
Topdevbeast <topdevbeast@gmail.com>
2022-07-28 13:44:05 -0700

    add swap rewards tasks

--- tasks/subtasks/swapRewards.js
+++ tasks/subtasks/swapRewards.js
@@ -12,49 +12,110 @@ const contract = async () => await hre.run("loadContract", { name: name, address
 // READ
 
 
-// helixToken
+subtask("swapRewards.helixToken")
+    .setAction(async () => {
+        const result = await (await contract()).helixToken()
+        console.log(result.toString())
+    })
 
-// oracleFactory
+subtask("swapRewards.oracleFactory")
+    .setAction(async () => {
+        const result = await (await contract()).oracleFactory()
+        console.log(result.toString())
+    })
 
-// owner
-subtask("helixToken.owner")
+subtask("swapRewards.owner")
     .setAction(async () => {
         const result = await (await contract()).owner()
         console.log(result.toString())
     })
 
+subtask("swapRewards.paused")
+    .setAction(async () => {
+        const result = await (await contract()).paused()
+        console.log(result.toString())
+    })
 
-// paused
+subtask("swapRewards.refReg")
+    .setAction(async () => {
+        const result = await (await contract()).refReg()
+        console.log(result.toString())
+    })
 
-// refReg
-
-// router
+subtask("swapRewards.router")
+    .setAction(async () => {
+        const result = await (await contract()).router()
+        console.log(result.toString())
+    })
 
 
 // WRITE
 
 
-// pause
+subtask("swapRewards.pause")
+    .setAction(async () => {
+        const result = await (await contract()).pause()
+    })
 
-// renounceOwnership
+subtask("swapRewards.renounceOwnership")
+    .setAction(async () => {
+        const result = await (await contract()).renounceOwnership()
+    })
 
-// setHelixToken
+subtask("swapRewards.setHelixToken")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setHelixToken(
+            args.arg0,
+        )
+    })
 
-// setOracleFactory
+subtask("swapRewards.setOracleFactory")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setOracleFactory(
+            args.arg0,
+        )
+    })
 
-// setRefReg
+subtask("swapRewards.setRefReg")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setRefReg(
+            args.arg0,
+        )
+    })
 
-// setRouter
+subtask("swapRewards.setRouter")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).setRouter(
+            args.arg0,
+        )
+    })
 
-// swap
-subtask("helixToken.swap")
+subtask("swapRewards.swap")
     .addPositionalParam("arg0")
     .addPositionalParam("arg1")
     .addPositionalParam("arg2")
     .setAction(async (args) => {
-        const result = await (await contract()).swap(args.arg0, args.arg1, args.arg2)
+        const result = await (await contract()).swap(
+            args.arg0, 
+            args.arg1, 
+            args.arg2
+        )
     })
 
-// transferOwnership
+subtask("swapRewards.transferOwnership")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).transferOwnership(
+            args.arg0,
+        )
+    })
 
-// unpause
+subtask("swapRewards.unpause")
+    .setAction(async () => {
+        const result = await (await contract()).unpause()
+    })
+
