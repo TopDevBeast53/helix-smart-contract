05e61ae3c8577331c492569bceb642829e2e1512
Topdevbeast <topdevbeast@gmail.com>
2022-07-28 14:12:12 -0700

    add multicall tasks

--- tasks/subtasks/multicall.js
+++ tasks/subtasks/multicall.js
@@ -12,35 +12,98 @@ const contract = async () => await hre.run("loadContract", { name: name, address
 // READ
 
 
-// getBlockHash
 subtask("multicall.getBlockHash")
     .setAction(async () => {
         const result = await (await contract()).getBlockHash()
         console.log(result.toString())
     })
 
-// getBlockNumber
+subtask("multicall.getBlockNumber")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).getBlockNumber(
+            args.arg0,
+        )
+        console.log(result.toString())
+    })
 
-// getCurrentBlockCoinbase
+subtask("multicall.getCurrentBlockCoinbase")
+    .setAction(async () => {
+        const result = await (await contract()).getCurrentBlockCoinbase()
+        console.log(result.toString())
+    })
 
-// getCurrentBlockDifficulty
+subtask("multicall.getCurrentBlockDifficulty")
+    .setAction(async () => {
+        const result = await (await contract()).getCurrentBlockDifficulty()
+        console.log(result.toString())
+    })
 
-// getCurrentBlockGasLimit
+subtask("multicall.getCurrentBlockGasLimit")
+    .setAction(async () => {
+        const result = await (await contract()).getCurrentBlockGasLimit()
+        console.log(result.toString())
+    })
 
-// getCurrentBlockTimestamp
+subtask("multicall.getCurrentBlockTimestamp")
+    .setAction(async () => {
+        const result = await (await contract()).getCurrentBlockTimestamp()
+        console.log(result.toString())
+    })
 
-// getEthBalance
+subtask("multicall.getEthBalance")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).getEthBalance(
+            args.arg0,
+        )
+        console.log(result.toString())
+    })
 
-// getLastBlockHash
+subtask("multicall.getLastBlockHash")
+    .setAction(async () => {
+        const result = await (await contract()).getLastBlockHash()
+        console.log(result.toString())
+    })
+
 
 
 // WRITE
 
 
-// aggregate
+subtask("multicall.aggregate")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).aggregate(
+            args.arg0,
+        )
+    })
 
-// blockAndAggregate
+subtask("multicall.blockAndAggregate")
+    .addPositionalParam("arg0")
+    .setAction(async (args) => {
+        const result = await (await contract()).blockAndAggregate(
+            args.arg0,
+        )
+    })
 
-// tryAggregate
+subtask("multicall.tryAggregate")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).tryAggregate(
+            args.arg0,
+            args.arg1,
+        )
+    })
 
-// tryBlockAndAggregate
+subtask("multicall.tryBlockAndAggregate")
+    .addPositionalParam("arg0")
+    .addPositionalParam("arg1")
+    .setAction(async (args) => {
+        const result = await (await contract()).tryBlockAndAggregate(
+            args.arg0,
+            args.arg1,
+        )
+    })
+
