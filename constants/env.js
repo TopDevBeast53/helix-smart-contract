f7bb9685b5a090fd0d8052b1fcb8af6a949f45e9
Topdevbeast <topdevbeast@gmail.com>
2022-07-29 11:58:33 -0700

    increase owner multisig confirmations

--- constants/env.js
+++ constants/env.js
@@ -1,17 +1,17 @@
 module.exports = {
     // network: 1,     // Ethereum Mainnet
     // network: 3,     // Ropsten Testnet
-    network: 4,     // Rinkeby Testnet
+    // network: 4,     // Rinkeby Testnet
     // network: 5,     // Goerli Testnet
-    // network: 31,    // RSK Testnet
+    network: 31,    // RSK Testnet
     // network: 56,    // BSC Mainnet
     // network: 97,    // BSC Testnet
 
     // rpcURL: 'https://eth-mainnet.g.alchemy.com/v2/VqUJXhZEYxUqofUHG6UWgxbuD3tw3yCf',    // Ethererum Mainnet
     // rpcURL: 'https://ropsten.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658',            // Ropsten Testnet
-    rpcURL: 'https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF',    // Rinkeby Testnet
+    // rpcURL: 'https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF',    // Rinkeby Testnet
     // rpcURL: 'https://goerli.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658',             // Goerli Testnet
-    // rpcURL: 'https://public-node.testnet.rsk.co/',                                      // RSK Testnet
+    rpcURL: 'https://public-node.testnet.rsk.co/',                                      // RSK Testnet
     // rpcURL: 'https://bsc-dataseed1.binance.org',                                        // BSC Mainnet
     // rpcURL: 'https://data-seed-prebsc-1-s1.binance.org:8545',                           // BSC Testnet
 
