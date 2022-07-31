6b1b0eeaf0770630fa188e0dc918eb9d206933bb
Topdevbeast <topdevbeast@gmail.com>
2022-07-31 09:05:00 +0800

    Change network with 30

--- constants/env.js
+++ constants/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    network: 1,     // Ethereum Mainnet
+    network: 30,     // Ethereum Mainnet
     // network: 3,     // Ropsten Testnet
     // network: 4,     // Rinkeby Testnet
     // network: 5,     // Goerli Testnet
@@ -8,7 +8,7 @@ module.exports = {
     // network: 56,    // BSC Mainnet
     // network: 97,    // BSC Testnet
 
-    rpcURL: 'https://eth-mainnet.g.alchemy.com/v2/VqUJXhZEYxUqofUHG6UWgxbuD3tw3yCf',    // Ethererum Mainnet
+    rpcURL: 'https://public-node.rsk.co',    // Ethererum Mainnet
     // rpcURL: 'https://ropsten.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658',            // Ropsten Testnet
     // rpcURL: 'https://eth-rinkeby.alchemyapi.io/v2/AZlNuxGblwVelxMo_E0rMTvVpl5H02XF',    // Rinkeby Testnet
     // rpcURL: 'https://goerli.infura.io/v3/ae4fd86582354ff0b957e9c8a0915658',             // Goerli Testnet
